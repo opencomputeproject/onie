e07654a16e5ba3ce2fe73784c1b16273e8636fad
Curt Brune <curt@cumulusnetworks.com>
2014-01-25 17:13:35 -0800

    mount_onie.sh: improve error checking
    Check that the host OS has the necessary utilities for extracting the
    ONIE initrd.  Be a bit more verbose in the face of errors.
    
    Reviewed-By: dustin

--- installer/x86_64/mount_onie.sh
+++ installer/x86_64/mount_onie.sh
@@ -8,7 +8,17 @@ mkdir -p $onie_dir || exit 1
 mount -t tmpfs -o "defaults,noatime,size=100M" onie-tmpfs /mnt/onie || exit 1
 
 cd $onie_dir
-xz -d -c $script_dir/onie.initrd | cpio -id
+# test that needed utilities are present in host OS
+for u in xz cpio ; do 
+    which $u > /dev/null 2>&1 || {
+        echo "ERROR: missing utility '$u', needed to mount ONIE tools"
+        exit 1
+    }
+done
+(xz -d -c $script_dir/onie.initrd | cpio -id) || {
+    echo "ERROR: problem extracting ONIE initrd for ONIE tools."
+    exit 1
+}
 
 echo "ONIE tools available in $onie_dir/bin"
 echo "Type 'umount $onie_dir' when finished"
