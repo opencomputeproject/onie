e63ec23c81634e8d7365e6f46533b842a9120d72
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-04-05 13:10:33 -0700

    New platforms.

--- builds/installer/powerpc/all/platforms.mk
+++ builds/installer/powerpc/all/platforms.mk
@@ -22,9 +22,17 @@
 # These are the platforms to include in this installer build.
 #
 ############################################################
-INSTALLER_PLATFORMS :=  powerpc-quanta-lb9-r0 \
-			powerpc-quanta-ly2-r0 \
+INSTALLER_PLATFORMS :=  powerpc-as4600-54t \
+			powerpc-as5600-52x \
 			powerpc-as5610-52x \
-			powerpc-as5600-52x
+			powerpc-as5710-54x-r0a \
+			powerpc-as6700-32x-r0 \
+			powerpc-quanta-lb9a-r0 \
+			powerpc-quanta-lb9-r0 \
+			powerpc-quanta-ly2-r0
+
+
+
+
 
 
