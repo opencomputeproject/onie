4fc7afc008e9f0ad713ac8d7eb742928ee52883b
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-04-05 13:26:49 -0700

    New platforms and kernel.

---
+++ components/all/platform-config/powerpc-as5710-54x-r0a/src/python/onlpc.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+############################################################
+#
+# Platform Driver for powerpc-as5710-54x-r0a
+#
+############################################################
+import os
+import struct
+import time
+import subprocess
+from onl.platform.base import *
+from onl.vendor.accton import *
+
+class OpenNetworkPlatformImplementation(OpenNetworkPlatformAccton):
+
+    def model(self):
+        return "AS5710-54X"
+
+    def platform(self):
+        return 'powerpc-as5710-54x-r0a'
+
+    def _plat_info_dict(self):
+        return {
+            platinfo.LAG_COMPONENT_MAX : 16,
+            platinfo.PORT_COUNT : 54
+            }
+
+    def _plat_oid_table(self):
+        return None
+
+    def get_environment(self):
+        return "Not implemented."
+
+
+
+if __name__ == "__main__":
+    import sys
+
+    p = OpenNetworkPlatformImplementation()
+    if len(sys.argv) == 1 or sys.argv[1] == 'info':
+        print p
+    elif sys.argv[1] == 'env':
+        print p.get_environment()
+
+
