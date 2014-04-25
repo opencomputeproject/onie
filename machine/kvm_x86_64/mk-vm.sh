cf214c98f8adf0c07d32c4cf8b305522d76f00bf
Curt Brune <curt@cumulusnetworks.com>
2014-04-25 11:22:09 -0700

    kvm_x86_64: update INSTALL instructions and VM creation
    Reviewed By: tbd
    
    Updated the instructions for creating a new ONIE virtual machine.
    
    Include new instructions for booting and embedding ONIE using the
    recovery ISO image.  This method is much easier than the iPXE install
    method.
    
    Also updated the mk-vm.sh script.  Three boot modes are supported:
    
    - boot from hard disk
    - boot from CDROM (recovery ISO)
    - boot from network (iPXE)

--- machine/kvm_x86_64/mk-vm.sh
+++ machine/kvm_x86_64/mk-vm.sh
@@ -1,10 +1,64 @@
 #!/bin/sh
 
-VM_NAME=onie-x86-test
 MEM=1024
-DISK="$HOME/kvm/onie-x86-demo.img,bus=virtio"
+DISK="$HOME/kvm/onie-x86-demo.img"
 
-sudo virt-install -n $VM_NAME -r $MEM \
---import --disk path="$DISK" \
---accelerate --network bridge=br0,model=e1000 \
---connect=qemu:///system --noautoconsole --vnc --hvm
+# Path to ONIE installer .iso image
+CDROM="$HOME/kvm/onie-recovery-x86_64-kvm_x86_64-r0.iso"
+
+# VM will listen on telnet port $KVM_PORT
+KVM_PORT=9000
+
+# VNC display to use
+VNC_PORT=0
+
+# set mode=disk to boot from hard disk
+# mode=disk
+
+# set mode=cdrom to boot from the CDROM
+mode=cdrom
+
+# set mode=net to boot from network adapters
+# mode=net
+
+on_exit()
+{
+    rm -f $kvm_log
+}
+
+kvm_log=$(mktemp)
+trap on_exit EXIT
+
+boot=c
+if [ "$mode" = "cdrom" ] ; then
+    boot="order=cd,once=d"
+    cdrom="-cdrom $CDROM"
+elif [ "$mode" = "net" ] ; then
+    boot="order=cd,once=n,menu=on"
+fi
+
+sudo /usr/bin/kvm -m $MEM \
+    -name "onie" \
+    -boot $boot $cdrom \
+    -net nic,model=e1000 \
+    -net tap,ifname=onie0 \
+    -vnc 0.0.0.0:$VNC_PORT \
+    -vga std \
+    -drive file=$DISK,media=disk,if=virtio,index=0 \
+    -serial telnet:localhost:$KVM_PORT,server > $kvm_log 2>&1 &
+
+kvm_pid=$!
+
+sleep 0.5
+
+[ -d "/proc/$kvm_pid" ] || {
+        echo "ERROR: kvm died."
+        cat $kvm_log
+        exit 1
+}
+
+telnet localhost $KVM_PORT
+
+echo "to kill kvm:  sudo kill $kvm_pid"
+
+exit 0
