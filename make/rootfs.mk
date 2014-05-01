0ee3f8b1321caa4c4b54cf8db6f4b6e49d878d1e
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-05-01 15:19:43 -0700

    Name change.

--- make/rootfs.mk
+++ make/rootfs.mk
@@ -1,22 +1,22 @@
 # -*- Makefile -*-
 ############################################################
 # <bsn.cl fy=2013 v=onl>
-# 
-#        Copyright 2013, 2014 Big Switch Networks, Inc.       
-# 
+#
+#        Copyright 2013, 2014 Big Switch Networks, Inc.
+#
 # Licensed under the Eclipse Public License, Version 1.0 (the
 # "License"); you may not use this file except in compliance
 # with the License. You may obtain a copy of the License at
-# 
+#
 #        http://www.eclipse.org/legal/epl-v10.html
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 # either express or implied. See the License for the specific
 # language governing permissions and limitations under the
 # License.
-# 
+#
 # </bsn.cl>
 ############################################################
 #
@@ -89,7 +89,7 @@ $(ROOTFS_BUILD_DIR)/.$(ROOTFS_NAME).done: $(PACKAGE_DEPENDENCY)
 	echo $$arch_repo ;\
 	sed "s%__DIR__%$(ONL_REPO)%g" $(ROOTFS_ARCH_REPO_PATH) >$$arch_repo ;\
 	sed "s%__DIR__%$(ONL_REPO)%g" $(ROOTFS_ALL_REPO_PATH) >$$all_repo ;\
-	$(ONL)/tools/scripts/mkws \
+	$(ONL)/tools/scripts/onl-mkws \
 	  -e ONL=$(ONL) \
 	  --apt-cache $(APT_CACHE) \
 	  --nested \
