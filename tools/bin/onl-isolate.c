e693c87529351ee65fb50898709e94906381f082
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-05-01 14:58:50 -0700

    The ONL workspaces tools are now prefixed with "onl-"

--- tools/bin/onl-isolate.c
+++ tools/bin/onl-isolate.c
@@ -1,21 +1,21 @@
 /*
  * <bsn.cl fy=2011 v=onl>
- * 
- *  Copyright 2011, 2012, 2013, 2014 Big Switch Networks, Inc. 
- * 
+ *
+ *  Copyright 2011, 2012, 2013, 2014 Big Switch Networks, Inc.
+ *
  * Licensed under the Eclipse Public License, Version 1.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *        http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
  * License.
- * 
+ *
  * </bsn.cl>
  *
  *
@@ -42,9 +42,9 @@ int main(int argc, char *argv[])
 {
     int help_flag = 0;
     int as_root_flag = 0;
-    char *pre_script = "isolate-pre";
-    char *child_script = "isolate-child";
-    char *post_script = "isolate-post";
+    char *pre_script = "onl-isolate-pre";
+    char *child_script = "onl-isolate-child";
+    char *post_script = "onl-isolate-post";
     char *chroot_arg = NULL;
     char *chdir_arg = NULL;
     int pid;
