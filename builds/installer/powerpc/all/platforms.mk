311de535bd7b5f5973ba985536070adfb6b69440
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-05-18 20:45:30 -0700

    New revisions.

--- builds/installer/powerpc/all/platforms.mk
+++ builds/installer/powerpc/all/platforms.mk
@@ -26,7 +26,9 @@ INSTALLER_PLATFORMS :=  powerpc-as4600-54t \
 			powerpc-as5600-52x \
 			powerpc-as5610-52x \
 			powerpc-as5710-54x-r0a \
+			powerpc-as5710-54x-r0b \
 			powerpc-as6700-32x-r0 \
+			powerpc-as6700-32x-r0b \
 			powerpc-quanta-lb9a-r0 \
 			powerpc-quanta-lb9-r0 \
 			powerpc-quanta-ly2-r0 \
