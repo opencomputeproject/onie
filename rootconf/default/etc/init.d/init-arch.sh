a5b38bd521c676315ee9e95ccdab13396eb5e50b
Curt Brune <curt@cumulusnetworks.com>
2014-06-18 17:48:27 -0700

    Allow for platform specific hardware init at boot time
    Reviewed By: dustin
    
    This patch updates the existing /etc/init.d/init-arch.sh script to
    also look for platform specific PRE and POST initialization handlers.
    
    The flow of init-arch.sh is now:
    
    1. run any platform specific PRE handler
    2. run the architecture specific handler
    3. run any platform specific POST handler
    
    These changes allow for machine specific hardware initialization early
    in the boot sequence.
    
    To make use of this feature create a small shell script fragment in
    the corresponding machine directory:
    
      machine/<vendor>/<machine_name>/rootconf/sysroot-lib-onie/init-platform
    
    In this file implement the functions init_platform_pre_arch() and
    init_platform_post_arch().
    
    The /etc/init.d/init-arch.sh script will call these routines at boot
    time.
    
    Testing Done:
    
    Added no-op (just echos a message) pre and post hanlders to the
    kvm_x86_64 and fsl_p2020rdbpca machines.  For both cases verified the
    no-op handlers execute during boot up.

--- rootconf/default/etc/init.d/init-arch.sh
+++ rootconf/default/etc/init.d/init-arch.sh
@@ -6,18 +6,33 @@ cmd="$1"
 
 . /lib/onie/functions
 
+init_platform_pre_arch()
+{
+    # NO-OP
+    true
+}
+
 init_arch()
 {
     # NO-OP
     true
 }
 
+init_platform_post_arch()
+{
+    # NO-OP
+    true
+}
+
 [ -r /lib/onie/init-arch ] && . /lib/onie/init-arch
+[ -r /lib/onie/init-platform ] && . /lib/onie/init-platform
 
 case $cmd in
     start)
+        init_platform_pre_arch
         init_arch
+        init_platform_post_arch
         ;;
     *)
-        
+
 esac
