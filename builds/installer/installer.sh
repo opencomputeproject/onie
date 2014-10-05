51b7ef78bb53207083133c0201d26c6b172bee70
Rob Sherwood <rob.sherwood@bigswitch.com>
2014-10-05 04:53:12 -0700

    Revert "Use only awk for umount match"
    This reverts commit 21474a6e887f42592f49215ad41a7184d0f43f87.
    
    This patch has some weird problem with the grep I don't understand,
    so I'm backing it out.
    
    ONIE:/ # sh -x onl-10183fe-powerpc-all.2014.10.05.03.52.installer
    [snip]
    + echo Platform installer version: unknown
    Platform installer version: unknown
    + platform_installer
    + installer_standard_blockdev_install sda 16M 64M
    + local blockdev=sda
    + local p1size=16M
    + local p2size=64M
    + local p3size=
    + installer_blockdev_format sda 16M 64M
    + local blockdev=sda
    + local partition1size=16M
    + local partition2size=64M
    + local partition3size=
    + installer_umount_blockdev sda
    + local blockdev=sda
    + echo Test Robs 0
    Test Robs 0
    + grep sda /proc/mounts
    onl-10183fe-powerpc-all.2014.10.05.03.52.installer: line 1: syntax error: unterminated quoted string

--- builds/installer/installer.sh
+++ builds/installer/installer.sh
@@ -162,9 +162,9 @@ installer_partition_format() {
 
 installer_umount_blockdev() { 
     local blockdev=$1
-    grep $blockdev /proc/mounts
+    grep $1 /proc/mounts
     if [ 0 = $? ]; then
-        umount `awk "/${blockdev}/ "'{print $2}' /proc/mounts` || true
+        umount `cat /proc/mounts | grep ${blockdev} | awk '{print $2}'` || true
     else
         echo $1 not mounted, skiping umount
         exit 1
