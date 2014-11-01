b1d816775aca54173c29e4b9acc988adc3e99ccb
Rob Sherwood <rob.sherwood@bigswitch.com>
2014-10-31 17:13:17 -0700

    FIX typos in ORC platform path

--- builds/swi/all/shared/etc/profile.d/onl.sh
+++ builds/swi/all/shared/etc/profile.d/onl.sh
@@ -1,10 +1,10 @@
 # Add platform specific directories to path
 
-dir=/lib/platform/`cat /etc/onl_platform`
+dir=/lib/platform-config/`cat /etc/onl_platform`
 
 if [ "`id -u`" -eq 0 ]; then
-    PATH="$PATH:$dir/bin"
-else
     PATH="$PATH:$dir/bin:$dir/sbin"
+else
+    PATH="$PATH:$dir/bin"
 fi
 export PATH
