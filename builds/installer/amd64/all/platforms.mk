77c4fd3e7237a8619fe15b0d9ea09300d423c3ce
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2015-03-09 12:30:40 -0700

    Initial AMD64 SWI build.

---
+++ builds/installer/amd64/all/platforms.mk
@@ -0,0 +1,35 @@
+############################################################
+# <bsn.cl fy=2013 v=onl>
+#
+#        Copyright 2013, 2014 Big Switch Networks, Inc.
+#
+# Licensed under the Eclipse Public License, Version 1.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#        http://www.eclipse.org/legal/epl-v10.html
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific
+# language governing permissions and limitations under the
+# License.
+#
+# </bsn.cl>
+############################################################
+#
+# These are the platforms to include in this installer build.
+#
+############################################################
+INSTALLER_PLATFORMS :=  x86-64-dell-s6000-s1220-r0 \
+			x86-64-dell-s4000-c2338-r0
+
+
+
+
+
+
+
+
+
