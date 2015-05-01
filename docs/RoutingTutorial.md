c596912c6544c8f220c983db6d675e2b3f6c7838
Rob Sherwood <rob.sherwood@bigswitch.com>
2015-04-30 19:27:48 -0700

    Updated for KVM-based Routing Tutorial

---
+++ docs/RoutingTutorial.md
@@ -0,0 +1,110 @@
+#ONL Routing Tutorial
+-------------------------------------------------
+
+The goal of the tutorial is to ping between virtual hosts connected via
+virtual routers.
+
+These instructions walk you through setting up a docker environment to
+run virtual ONL KVM instances and Quagga in a virtual network and 
+configure BGP/OSPF etc. routes, peering, and other fun things.
+
+
+#Host Requirements
+-------------------------------------------------
+
+This tutorial assumes that you have access to a Linux host (physical
+server or VM) that is modern enough to run docker.  This typically means
+a kernel newer than 3.10.x.
+
+Requirements:
+- docker v1.0 or above 
+- About 3GB of free disk space
+- About 4GB of RAM
+
+
+# Tutorial Overview
+-------------------------------------------------
+
+The goal of the tutorial is to ping between virtual hosts connected via
+virtual routers.  With ONL, this would be much more interesting with 
+physical hosts and physical routers (e.g., using `orc`), but virtually is
+easier to setup for self-guided tutorial.  In principle, many of these same
+steps will work with a physical setup.
+
+1. Download and install pre-built docker image 'opennetworklinux/routing-tutorial'
+2. Run the kvm-router-demo.sh script to spawn the virtual topology
+3. Configure quagga on the corresponding routers so that they peer
+4. Verify that the virtual hosts can ping each other through the routers
+
+
+# Download Tutorial Image
+-------------------------------------------------
+
+If your docker installation is working, this should be easy:
+
+    docker.io pull onl/routing-tutorial
+    docker.io run -i -t --privileged  \
+        -h "tutorial" \
+        opennetworklinux/routing-tutorial 
+
+If you do not have docker installed on your system, consider
+    sudo apt-get install docker.io
+
+or
+    wget -qO- https://get.docker.com/ | sh
+
+Also, your docker binary might be called 'docker' not 'docker.io'
+depending on your system. 
+
+
+
+# Start Up Virtual Hosts and Routers
+-------------------------------------------------
+
+    ./kvm-router-demo.sh -setup         # launch everything
+    tmux a 
+    
+If you take a look at the kvm-router-demo.sh script, it does lots
+of things:
+
+* Creates .img files for each KVM router
+* Creates lots of Linux containers for virtual hosts and routers
+* Creates Linux bridges to act as virtual links between hosts and routers
+* Assigns IP addresses to the virtual hosts
+* Spawns a KVM instance with the console connected to `tmux` for each router
+
+The script also supports the '-teardown' option to undo everything is setup (though
+exiting the docker shell is probably cleaner) and '-show' to show the status
+of various tutorial elements.
+
+
+# Configuring the Routers and Hosts Networks
+
+Now we configure our network to look like the reference topology:
+
+![Tutorial Topology](https://github.com/opennetworklinux/ONL/tools/docker.tutorial/topology.png "Tutorial Topology")
+
+
+From tmux, hit ctl+b and then '1' to go to the first router, login as root
+(password 'onl'), and then cut and paste these commands:
+
+    hostname router1
+    exec bash
+    ifconfig eth1 10.99.1.3 netmask 255.255.255.0
+    ifconfig eth2 10.99.3.2 netmask 255.255.255.0
+
+And then confirm that R1 can reach H1 with:
+    
+    ping 10.99.1.2
+
+Now use ctl+b and then '2' to switch to route2 and execute the equivalent commands:
+
+    hostname router2
+    exec bash
+    ifconfig eth1 10.99.3.3 netmask 255.255.255.0
+    ifconfig eth2 10.99.2.3 netmask 255.255.255.0
+
+And then confirm that R2 can reach H2 and R1 with:
+    
+    ping 10.99.2.2       # can we reach H2?
+    ping 10.99.3.2       # can we reach R1?
--- tools/kvm-router-demo.sh
+++ tools/kvm-router-demo.sh
@@ -18,16 +18,37 @@ BRCTL=brctl
 IP=ip
 KVM=kvm
 
-SCREEN=demo
+SCREEN=tutorial
 BRIDGES="br-h1-r1 br-r1-r2 br-r2-h2"
 INTERFACES="h1-eth1 h2-eth1 $BRIDGES"
-NAMESPACES="h1 h2 r1 r2"
+NAMESPACES="h1 h2"
 PREFIX="10.99"
+SWI=onl-i386-kvm.swi
 SRC_KVM_IMG=onl-i386.img
 #KVM_OPTS="-m 1024 -cdrom loader-i386.iso -boot d -nographic -hda onl-i386.img"
 KVM_OPTS="-m 1024 -cdrom loader-i386.iso -boot d -nographic -chardev pty,id=pty0 "
 
 do_setup() {
+
+   # shipping the .swi is much smaller than shipping the .img!
+   if [ ! -f $SRC_KVM_IMG ] ; then
+           echo Making $SRC_KVM_IMG from $SWI
+           mkdosfs -F 32 -C $SRC_KVM_IMG 1000000
+           mcopy -i $SRC_KVM_IMG $SWI ::onl-i386.swi
+           echo "SWI=flash:onl-i386.swi" > boot-config
+           echo "NETDEV=ma1" >> boot-config
+           echo "NETAUTO=dhcp" >> boot-config
+           mcopy -i $SRC_KVM_IMG boot-config ::boot-config
+           rm boot-config
+   fi
+
+   if [ ! -f onl-r1.img ] ; then
+        cp $SRC_KVM_IMG onl-r1.img
+   fi
+
+   if [ ! -f onl-r12img ] ; then
+        cp $SRC_KVM_IMG onl-r2.img
+   fi
 
    for bridge in $BRIDGES; do 
       echo Adding bridge $bridge
@@ -72,23 +93,30 @@ do_setup() {
    $IP addr change ${PREFIX}.3.1 broadcast ${PREFIX}.3.255 dev br-r1-r2
    $IP route add ${PREFIX}.3.0/24 dev br-r1-r2
 
-   echo Starting ONL image Router1
-   screen -S $SCREEN -d -m $KVM $KVM_OPTS \
-	-name router1 \
-	-vnc :0 \
-	-net nic -net user,net=${PREFIX}.14.0/24,hostname=router1 \
-	-net nic -net tap,ifname=r1-eth0,script=no,downscript=no \
-	-net nic -net tap,ifname=r1-eth1,script=no,downscript=no \
-	-hda onl-r1.img 
+    tmux new -s $SCREEN \; detach
+    echo Starting ONL image Router1
+    tmux new-window -n router1 "$KVM $KVM_OPTS \
+            -name router1 \
+            -vnc :0 \
+            -net nic -net user,net=${PREFIX}.14.0/24,hostname=router1 \
+            -net nic -net tap,ifname=r1-eth0,script=no,downscript=no \
+            -net nic -net tap,ifname=r1-eth1,script=no,downscript=no \
+            -hda onl-r1.img"
 
    echo Starting ONL image Router2
-   screen -S $SCREEN -X screen $KVM $KVM_OPTS \
-	-name router2 \
-	-vnc :1 \
-	-net nic -net user,net=${PREFIX}.24.0/24,hostname=router2 \
-	-net nic -net tap,ifname=r2-eth0,script=no,downscript=no \
-	-net nic -net tap,ifname=r2-eth1,script=no,downscript=no \
-	-hda onl-r2.img 
+   tmux new-window -n router2 "$KVM $KVM_OPTS \
+            -name router2 \
+            -vnc :1 \
+            -net nic -net user,net=${PREFIX}.24.0/24,hostname=router2 \
+            -net nic -net tap,ifname=r2-eth0,script=no,downscript=no \
+            -net nic -net tap,ifname=r2-eth1,script=no,downscript=no \
+            -hda onl-r2.img"
+
+   echo Starting Shell for H1
+   tmux new-window -n H1 "ip netns exec H1 bash"
+
+   echo Starting Shell for H2
+   tmux new-window -n H2 "ip netns exec H2 bash"
 
    echo Waiting a bit for KVM to start
    sleep 2
@@ -104,27 +132,29 @@ do_setup() {
 
 do_teardown() {
 
-   echo '*** ' Killing all KVM instances
-   killall qemu-system-x86_64
+    echo '*** ' Killing all KVM instances
+    killall qemu-system-x86_64
 
-   echo '*** ' Bringing down all interfaces
-   for intf in $INTERFACES ; do 
-      $IP link set dev $intf down
-   done
-   for bridge in $BRIDGES; do 
-      echo '*** ' Removing bridge $bridge
-      $BRCTL delbr $bridge 
-   done
+    echo '*** ' Bringing down all interfaces
+    for intf in $INTERFACES ; do 
+       $IP link set dev $intf down
+    done
+    for bridge in $BRIDGES; do 
+       echo '*** ' Removing bridge $bridge
+       $BRCTL delbr $bridge 
+    done
 
-   echo '*** ' Removing Namespaces
-   for ns in $NAMESPACES ; do 
-       echo '*** ' Removing namespace $ns
-       $IP netns delete $ns
-   done
+    echo '*** ' Removing Namespaces
+    for ns in $NAMESPACES ; do 
+        echo '*** ' Removing namespace $ns
+        $IP netns delete $ns
+    done
 
-   echo '*** ' Removing screen \'$SCREEN\'
-   screen -wipe $SCREEN
+    echo '*** ' Removing screen \'$SCREEN\'
+    screen -wipe $SCREEN
 	
+    echo '*** ' Removing Router KVM images
+    rm onl-r1.img onl-r2.img
 
    # Not needed anymore
    #echo Removing h1 interfaces
@@ -147,6 +177,7 @@ do_show () {
     pgrep qemu-system-x86_6
     echo '*** ' Screen instances
     screen -ls
+
 }
 
 die () {
