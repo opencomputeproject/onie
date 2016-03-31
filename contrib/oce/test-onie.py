2d2cbc16f7967e6b379532ad2a1563c95518c62b
Carlos Cardenas <carlos@cumulusnetworks.com>
2016-03-31 13:04:59 -0700

    Refactor out some constants for other modules
    Created 'oce' module with common constants to be used by
    other modules to be introduced shortly.
    
    Signed-off-by: Carlos Cardenas <carlos@cumulusnetworks.com>

--- contrib/oce/test-onie.py
+++ contrib/oce/test-onie.py
@@ -1,37 +1,21 @@
 #!/usr/bin/env python
 
+# -----------------------------------------------------------------------------
+# Copyright (C) 2014-2016 Carlos Cardenas <carlos@cumulusnetworks.com>
+#
+# SPDX-License-Identifier:     GPL-2.0
+#
+# -----------------------------------------------------------------------------
+
 import logging
 import os
 import os.path
 import re
 import sys
 
-OPTIONS = {'onie_installer': str,
-           'onie_updater': str,
-           'onie_arch': str,
-           'onie_switch_asic': str,
-           'onie_vendor': str,
-           'onie_machine': str,
-           'onie_machine_rev': str,
-           'http_binary': str,
-           'http_user': str,
-           'http_group': str,
-           'http_port': int,
-           'tftp_binary': str,
-           'tftp_user': str,
-           'tftp_group': str,
-           'dhcp_binary': str,
-           'dhcp_user': str,
-           'dhcp_group': str,
-           'dhcp_lease_time': int,
-           'dhcp_max_lease_time': int,
-           'dhcp_domain_name': str,
-           'dhcp_dns_server': str,
-           'dhcp_gateway': str,
-           'dhcp_next_server': str}
-PRETTY_OPTIONS = sorted(OPTIONS.keys())
+from oce import OPTIONS, PRETTY_OPTIONS, MAC_REGEX
+
 LOADED_MODULES = {}
-MAC_REGEX = re.compile('([a-fA-F0-9]{2}[:]?){6}')
 TEST_DEFINE = {}
 DUT_CONFIG = {}
 DEFAULT_OUTPUT_DIR = 'output'
