opencomputeproject/onie

commit 44a1f46a758093338cc6450796bd1a72b27484b7

Update Crosstool-NG from 1.21.0 to support Aarch64
Signed-off-by: Curt Brune <curt@cumulusnetworks.com>

build-config/make/images.make
@@ -129,17 +129,29 @@ CHECKDIR	= $(CHECKROOT)/checkdir
 CHECKFILES	= $(CHECKROOT)/checkfiles.txt
 SYSFILES	= $(CHECKROOT)/sysfiles.txt
 
-SYSROOT_LIBS	= ld$(CLIB64)-uClibc.so.0 ld$(CLIB64)-uClibc-$(UCLIBC_VERSION).so \
-		  libm.so.0 libm-$(UCLIBC_VERSION).so \
+ifeq ($(XTOOLS_LIBC),uClibc)
+SYSROOT_LIBS	= ld$(CLIB64)-uClibc.so.0 ld$(CLIB64)-uClibc-$(XTOOLS_LIBC_VERSION).so \
+		  libm.so.0 libm-$(XTOOLS_LIBC_VERSION).so \
 		  libgcc_s.so.1 libgcc_s.so \
-		  libc.so.0 libuClibc-$(UCLIBC_VERSION).so \
-		  libcrypt.so.0 libcrypt-$(UCLIBC_VERSION).so \
-		  libutil.so.0 libutil-$(UCLIBC_VERSION).so
-
-ifeq ($(EXT3_4_ENABLE),yes)
-SYSROOT_LIBS	+= \
-		  libdl.so.0 libdl-$(UCLIBC_VERSION).so \
-		  libpthread.so.0 libpthread-$(UCLIBC_VERSION).so
+		  libc.so.0 libuClibc-$(XTOOLS_LIBC_VERSION).so \
+		  libcrypt.so.0 libcrypt-$(XTOOLS_LIBC_VERSION).so \
+		  libutil.so.0 libutil-$(XTOOLS_LIBC_VERSION).so \
+	     	  libdl.so.0 libdl-$(XTOOLS_LIBC_VERSION).so \
+		  libpthread.so.0 libpthread-$(XTOOLS_LIBC_VERSION).so \
+		  librt.so.0 librt-$(XTOOLS_LIBC_VERSION).so
+else ifeq ($(XTOOLS_LIBC),glibc)
+SYSROOT_LIBS	= ld-$(XTOOLS_LIBC_VERSION).so \
+		  libm.so.6 libm-$(XTOOLS_LIBC_VERSION).so \
+		  libgcc_s.so.1 libgcc_s.so \
+		  libc.so.6 libc-$(XTOOLS_LIBC_VERSION).so \
+		  libcrypt.so.1 libcrypt-$(XTOOLS_LIBC_VERSION).so \
+		  libutil.so.1 libutil-$(XTOOLS_LIBC_VERSION).so \
+		  libdl.so.2 libdl-$(XTOOLS_LIBC_VERSION).so \
+		  libpthread.so.0 libpthread-$(XTOOLS_LIBC_VERSION).so \
+		  librt.so.1 librt-$(XTOOLS_LIBC_VERSION).so
+  ifeq ($(ARCH),arm64)
+    SYSROOT_LIBS	+= ld-linux-aarch64.so.1
+  endif
 endif
 
 ifeq ($(REQUIRE_CXX_LIBS),yes)
@@ -151,11 +163,6 @@ ifeq ($(REQUIRE_CXX_LIBS),yes)
   else
     $(error C++ support: Unsupported GCC version: $(GCC_VERSION))
   endif
-endif
-
-# Add librt if ACPI or LVM2 is enabled
-ifneq ($(filter yes, $(ACPI_ENABLE) $(LVM2_ENABLE)),)
-  SYSROOT_LIBS += librt.so.0 librt-$(UCLIBC_VERSION).so
 endif
 
 # Optionally add debug utilities
@@ -179,11 +186,17 @@ endif
 sysroot-check: $(SYSROOT_CHECK_STAMP)
 $(SYSROOT_CHECK_STAMP): $(PACKAGES_INSTALL_STAMPS)
 	$(Q) for file in $(SYSROOT_LIBS) ; do \
-		[ -r "$(DEV_SYSROOT)/lib/$$file" ] || { \
-			echo "ERROR: Missing SYSROOT_LIB: $$file" ; \
-			exit 1; } ; \
-		find $(DEV_SYSROOT)/lib -name $$file | xargs -i \
-		cp -av {} $(SYSROOTDIR)/lib/ || exit 1 ; \
+		if [ "$(ARCH)" == "arm64" ] || [ "$(ARCH)" == "x86_64" ] ; then \
+		    [ -r "$(DEV_SYSROOT)/lib64/$$file" ] || [ -r "$(DEV_SYSROOT)/lib/$$file" ] || { \
+			    echo "ERROR: Missing SYSROOT_LIB: $$file" ; \
+			    exit 1; } ; \
+			find $(DEV_SYSROOT)/lib64 $(DEV_SYSROOT)/lib -name $$file | xargs -i cp -av {} $(SYSROOTDIR)/lib/ || exit 1 ; \
+		else \
+		    [ -r "$(DEV_SYSROOT)/lib/$$file" ] || { \
+			    echo "ERROR: Missing SYSROOT_LIB: $$file" ; \
+			    exit 1; } ; \
+			find $(DEV_SYSROOT)/lib -name $$file | xargs -i cp -av {} $(SYSROOTDIR)/lib/ || exit 1 ; \
+		fi; \
 	done
 	$(Q) for file in $(DEBUG_UTILS) ; do \
 		cp -av $$file $(SYSROOTDIR)/usr/bin || exit 1 ; \

build-config/Makefile
@@ -5,6 +5,7 @@
 #  Copyright (C) 2014,2015 Carlos Cardenas <carlos@cumulusnetworks.com>
 #  Copyright (C) 2014 david_yang <david_yang@accton.com>
 #  Copyright (C) 2014 Mandeep Sandhu <mandeep.sandhu@cyaninc.com>
+#  Copyright (C) 2016 Pankaj Bansal <pankajbansal3073@gmail.com>
 #
 #  SPDX-License-Identifier:     GPL-2.0
 #
@@ -290,7 +291,9 @@ ONIE_MIRROR	?= http://mirror.opencompute.org/onie
 
 include make/crosstool-ng.make
 include make/kernel-download.make
-include make/uclibc-download.make
+ifneq ($(XTOOLS_LIBC),glibc)
+  include make/uclibc-download.make
+endif
 include make/xtools.make
 include make/sysroot.make
 include make/kernel.make

build-config/conf/crosstool/gcc-4.9.2/glibc-2.24/crosstool.armv8a.config
@@ -88,23 +88,29 @@ CT_LOG_FILE_COMPRESS=y
 #
 # Target options
 #
-CT_ARCH="x86"
+CT_ARCH="arm"
+CT_ARCH_SUPPORTS_BOTH_MMU=y
+CT_ARCH_SUPPORTS_BOTH_ENDIAN=y
 CT_ARCH_SUPPORTS_32=y
 CT_ARCH_SUPPORTS_64=y
 CT_ARCH_SUPPORTS_WITH_ARCH=y
 CT_ARCH_SUPPORTS_WITH_CPU=y
 CT_ARCH_SUPPORTS_WITH_TUNE=y
+CT_ARCH_DEFAULT_HAS_MMU=y
+CT_ARCH_DEFAULT_LE=y
 CT_ARCH_DEFAULT_32=y
 CT_ARCH_ARCH=""
 CT_ARCH_CPU=""
 CT_ARCH_TUNE=""
+# CT_ARCH_BE is not set
+CT_ARCH_LE=y
 # CT_ARCH_32 is not set
 CT_ARCH_64=y
 CT_ARCH_BITNESS=64
 CT_TARGET_CFLAGS=""
 CT_TARGET_LDFLAGS=""
 # CT_ARCH_alpha is not set
-# CT_ARCH_arm is not set
+CT_ARCH_arm=y
 # CT_ARCH_avr is not set
 # CT_ARCH_m68k is not set
 # CT_ARCH_microblaze is not set
@@ -114,7 +120,7 @@ CT_TARGET_LDFLAGS=""
 # CT_ARCH_s390 is not set
 # CT_ARCH_sh is not set
 # CT_ARCH_sparc is not set
-CT_ARCH_x86=y
+# CT_ARCH_x86 is not set
 # CT_ARCH_xtensa is not set
 CT_ARCH_alpha_AVAILABLE=y
 CT_ARCH_arm_AVAILABLE=y
@@ -136,11 +142,23 @@ CT_ARCH_SUFFIX=""
 #
 # CT_MULTILIB is not set
 CT_ARCH_USE_MMU=y
+CT_ARCH_ENDIAN="little"
 
 #
 # Target optimisations
 #
+CT_ARCH_EXCLUSIVE_WITH_CPU=y
 CT_ARCH_FLOAT=""
+
+#
+# arm other options
+#
+CT_ARCH_ARM_MODE="arm"
+CT_ARCH_ARM_MODE_ARM=y
+# CT_ARCH_ARM_MODE_THUMB is not set
+# CT_ARCH_ARM_INTERWORKING is not set
+CT_ARCH_ARM_EABI_FORCE=y
+CT_ARCH_ARM_EABI=y
 
 #
 # Toolchain options
@@ -163,7 +181,7 @@ CT_TOOLCHAIN_BUGURL=""
 #
 CT_TARGET_VENDOR="onie"
 CT_TARGET_ALIAS_SED_EXPR=""
-CT_TARGET_ALIAS="x86_64"
+CT_TARGET_ALIAS="armv8a"
 
 #
 # Toolchain type
@@ -194,7 +212,6 @@ CT_KERNEL="linux"
 CT_KERNEL_VERSION="${LINUX_RELEASE}"
 # CT_KERNEL_bare_metal is not set
 CT_KERNEL_linux=y
-# CT_KERNEL_windows is not set
 CT_KERNEL_bare_metal_AVAILABLE=y
 CT_KERNEL_linux_AVAILABLE=y
 CT_KERNEL_LINUX_CUSTOM=y
@@ -241,8 +258,8 @@ CT_BINUTILS_GOLD_SUPPORTS_ARCH=y
 CT_BINUTILS_GOLD_SUPPORT=y
 CT_BINUTILS_HAS_PLUGINS=y
 CT_BINUTILS_HAS_PKGVERSION_BUGURL=y
+CT_BINUTILS_FORCE_LD_BFD=y
 CT_BINUTILS_LINKER_LD=y
-# CT_BINUTILS_LINKER_GOLD is not set
 # CT_BINUTILS_LINKER_LD_GOLD is not set
 # CT_BINUTILS_LINKER_GOLD_LD is not set
 CT_BINUTILS_LINKERS_LIST="ld"
@@ -260,57 +277,50 @@ CT_BINUTILS_FOR_TARGET_BFD=y
 #
 # C-library
 #
-CT_LIBC="uClibc"
-CT_LIBC_VERSION="0.9.33.2"
-# CT_LIBC_glibc is not set
+CT_LIBC="glibc"
+CT_LIBC_VERSION="${XTOOLS_LIBC_VERSION}"
+CT_LIBC_glibc=y
 # CT_LIBC_musl is not set
-CT_LIBC_uClibc=y
+# CT_LIBC_uClibc is not set
 CT_LIBC_avr_libc_AVAILABLE=y
 CT_LIBC_glibc_AVAILABLE=y
 CT_THREADS="nptl"
+CT_LIBC_GLIBC_CUSTOM=y
+CT_LIBC_GLIBC_CUSTOM_LOCATION="${DOWNLOADDIR}/${XTOOLS_LIBC}-${XTOOLS_LIBC_VERSION}.tar.xz"
+CT_LIBC_GLIBC_CUSTOM_VERSION="${XTOOLS_LIBC_VERSION}"
+CT_LIBC_GLIBC_2_20_or_later=y
 CT_LIBC_mingw_AVAILABLE=y
 CT_LIBC_musl_AVAILABLE=y
 CT_LIBC_newlib_AVAILABLE=y
 CT_LIBC_none_AVAILABLE=y
 CT_LIBC_uClibc_AVAILABLE=y
-# CT_LIBC_UCLIBC_CUSTOM is not set
-# CT_LIBC_UCLIBC_CUSTOM_UCLIBC is not set
-# CT_LIBC_UCLIBC_CUSTOM_UCLIBC_NG is not set
-# CT_LIBC_UCLIBC_NG_V_1_0_12 is not set
-CT_LIBC_UCLIBC_V_0_9_33_2=y
-CT_LIBC_UCLIBC_0_9_33_2_or_later=y
-CT_LIBC_UCLIBC_PARALLEL=y
-CT_LIBC_UCLIBC_VERBOSITY_0=y
-# CT_LIBC_UCLIBC_VERBOSITY_1 is not set
-# CT_LIBC_UCLIBC_VERBOSITY_2 is not set
-CT_LIBC_UCLIBC_VERBOSITY=""
-CT_LIBC_UCLIBC_DEBUG_LEVEL_0=y
-# CT_LIBC_UCLIBC_DEBUG_LEVEL_1 is not set
-# CT_LIBC_UCLIBC_DEBUG_LEVEL_2 is not set
-# CT_LIBC_UCLIBC_DEBUG_LEVEL_3 is not set
-CT_LIBC_UCLIBC_DEBUG_LEVEL=0
-CT_LIBC_UCLIBC_CONFIG_FILE="${UCLIBC_CONFIG}"
 CT_LIBC_SUPPORT_THREADS_ANY=y
 CT_LIBC_SUPPORT_THREADS_NATIVE=y
-CT_LIBC_SUPPORT_THREADS_LT=y
-CT_LIBC_SUPPORT_THREADS_NONE=y
 
 #
 # Common C library options
 #
 CT_THREADS_NATIVE=y
-# CT_THREADS_LT is not set
-# CT_THREADS_NONE is not set
 CT_LIBC_XLDD=y
 
 #
-# uClibc other options
+# glibc other options
 #
-CT_LIBC_UCLIBC_LNXTHRD=""
-# CT_LIBC_UCLIBC_LOCALES is not set
-CT_LIBC_UCLIBC_IPV6=y
-CT_LIBC_UCLIBC_WCHAR=y
-CT_LIBC_UCLIBC_FENV=y
+# CT_LIBC_GLIBC_PORTS_EXTERNAL is not set
+CT_LIBC_glibc_familly=y
+CT_LIBC_GLIBC_EXTRA_CONFIG_ARRAY=""
+CT_LIBC_GLIBC_CONFIGPARMS=""
+CT_LIBC_GLIBC_EXTRA_CFLAGS=""
+# CT_LIBC_ENABLE_FORTIFIED_BUILD is not set
+# CT_LIBC_DISABLE_VERSIONING is not set
+CT_LIBC_OLDEST_ABI=""
+CT_LIBC_GLIBC_FORCE_UNWIND=y
+CT_LIBC_ADDONS_LIST=""
+# CT_LIBC_LOCALES is not set
+# CT_LIBC_GLIBC_KERNEL_VERSION_NONE is not set
+CT_LIBC_GLIBC_KERNEL_VERSION_AS_HEADERS=y
+# CT_LIBC_GLIBC_KERNEL_VERSION_CHOSEN is not set
+CT_LIBC_GLIBC_MIN_KERNEL="${LINUX_RELEASE}"
 
 #
 # C compiler
@@ -358,6 +368,7 @@ CT_CC_GCC_ENABLE_TARGET_OPTSPACE=y
 # CT_CC_GCC_LIBGOMP is not set
 # CT_CC_GCC_LIBSSP is not set
 # CT_CC_GCC_LIBQUADMATH is not set
+# CT_CC_GCC_LIBSANITIZER is not set
 
 #
 # Misc. obscure options.
@@ -397,11 +408,7 @@ CT_CC_LANG_OTHERS=""
 # Debug facilities
 #
 # CT_DEBUG_dmalloc is not set
-CT_DEBUG_duma=y
-CT_DUMA_A=y
-CT_DUMA_SO=y
-CT_DUMA_V_2_5_15=y
-CT_DUMA_VERSION="2_5_15"
+# CT_DEBUG_duma is not set
 CT_DEBUG_gdb=y
 CT_GDB_CROSS=y
 # CT_GDB_CROSS_STATIC is not set
@@ -445,11 +452,7 @@ CT_GDB_7_0_or_later=y
 CT_GDB_HAS_PKGVERSION_BUGURL=y
 CT_GDB_HAS_PYTHON=y
 CT_GDB_INSTALL_GDBINIT=y
-CT_DEBUG_ltrace=y
-CT_LTRACE_V_0_7_3=y
-# CT_LTRACE_V_0_5_3 is not set
-# CT_LTRACE_V_0_5_2 is not set
-CT_LTRACE_VERSION="0.7.3"
+# CT_DEBUG_ltrace is not set
 CT_DEBUG_strace=y
 # CT_STRACE_V_4_10 is not set
 CT_STRACE_V_4_9=y
@@ -465,6 +468,8 @@ CT_STRACE_VERSION="4.9"
 # Companion libraries
 #
 CT_COMPLIBS_NEEDED=y
+CT_LIBICONV_NEEDED=y
+CT_GETTEXT_NEEDED=y
 CT_GMP_NEEDED=y
 CT_MPFR_NEEDED=y
 CT_ISL_NEEDED=y
@@ -472,13 +477,18 @@ CT_MPC_NEEDED=y
 CT_EXPAT_NEEDED=y
 CT_NCURSES_NEEDED=y
 CT_COMPLIBS=y
+CT_LIBICONV=y
+CT_GETTEXT=y
 CT_GMP=y
 CT_MPFR=y
 CT_ISL=y
 CT_MPC=y
-CT_LIBELF_TARGET=y
 CT_EXPAT=y
 CT_NCURSES=y
+CT_LIBICONV_V_1_14=y
+CT_LIBICONV_VERSION="1.14"
+CT_GETTEXT_V_0_19_6=y
+CT_GETTEXT_VERSION="0.19.6"
 # CT_GMP_V_6_1_0 is not set
 CT_GMP_V_6_0_0=y
 # CT_GMP_V_5_1_3 is not set
@@ -512,13 +522,6 @@ CT_MPC_V_1_0_2=y
 # CT_MPC_V_0_8_1 is not set
 # CT_MPC_V_0_7 is not set
 CT_MPC_VERSION="1.0.2"
-
-#
-# libelf version needed to build for target
-#
-CT_LIBELF_V_0_8_13=y
-# CT_LIBELF_V_0_8_12 is not set
-CT_LIBELF_VERSION="0.8.13"
 CT_EXPAT_V_2_1_0=y
 CT_EXPAT_VERSION="2.1.0"
 CT_NCURSES_V_6_0=y

build-config/conf/crosstool/gcc-4.9.2/uClibc-0.9.33.2/crosstool.armv7a.config
@@ -1,11 +1,11 @@
 #
-# Automatically generated make config: don't edit
-# crosstool-NG 1.21.0 Configuration
-# Tue Sep  1 11:52:57 2015
+# Automatically generated file; DO NOT EDIT.
+# Crosstool-NG Configuration
 #
+CT_CONFIGURE_has_static_link=y
 CT_CONFIGURE_has_make381=y
 CT_CONFIGURE_has_xz=y
-CT_CONFIGURE_has_svn=y
+CT_CONFIGURE_has_cvs=y
 CT_MODULES=y
 
 #
@@ -17,7 +17,6 @@ CT_MODULES=y
 #
 # CT_OBSOLETE is not set
 CT_EXPERIMENTAL=y
-# CT_EXPERIMENTAL_PATCHES is not set
 # CT_ALLOW_BUILD_AS_ROOT is not set
 # CT_DEBUG_CT is not set
 
@@ -25,15 +24,15 @@ CT_EXPERIMENTAL=y
 # Paths
 #
 CT_LOCAL_TARBALLS_DIR="${DOWNLOADDIR}"
-# CT_SAVE_TARBALLS is not set
-CT_CUSTOM_LOCATION_ROOT_DIR=""
+CT_SAVE_TARBALLS=y
 CT_WORK_DIR="${CT_TOP_DIR}/build"
 CT_PREFIX_DIR="${XTOOLS_INSTALL_DIR}/${CT_TARGET}"
 CT_INSTALL_DIR="${CT_PREFIX_DIR}"
 CT_RM_RF_PREFIX_DIR=y
 CT_REMOVE_DOCS=y
 CT_INSTALL_DIR_RO=y
-CT_STRIP_ALL_TOOLCHAIN_EXECUTABLES=y
+CT_STRIP_HOST_TOOLCHAIN_EXECUTABLES=y
+CT_STRIP_TARGET_TOOLCHAIN_EXECUTABLES=y
 
 #
 # Downloading
@@ -103,9 +102,7 @@ CT_ARCH_SUPPORTS_SOFTFP=y
 CT_ARCH_DEFAULT_HAS_MMU=y
 CT_ARCH_DEFAULT_LE=y
 CT_ARCH_DEFAULT_32=y
-CT_ARCH_ARCH="armv7-a"
 CT_ARCH_CPU="cortex-a9"
-CT_ARCH_TUNE="cortex-a9"
 CT_ARCH_FPU=""
 # CT_ARCH_BE is not set
 CT_ARCH_LE=y
@@ -118,8 +115,7 @@ CT_TARGET_CFLAGS=""
 CT_TARGET_LDFLAGS=""
 # CT_ARCH_alpha is not set
 CT_ARCH_arm=y
-# CT_ARCH_avr32 is not set
-# CT_ARCH_blackfin is not set
+# CT_ARCH_avr is not set
 # CT_ARCH_m68k is not set
 # CT_ARCH_microblaze is not set
 # CT_ARCH_mips is not set
@@ -129,10 +125,10 @@ CT_ARCH_arm=y
 # CT_ARCH_sh is not set
 # CT_ARCH_sparc is not set
 # CT_ARCH_x86 is not set
+# CT_ARCH_xtensa is not set
 CT_ARCH_alpha_AVAILABLE=y
 CT_ARCH_arm_AVAILABLE=y
-CT_ARCH_avr32_AVAILABLE=y
-CT_ARCH_blackfin_AVAILABLE=y
+CT_ARCH_avr_AVAILABLE=y
 CT_ARCH_m68k_AVAILABLE=y
 CT_ARCH_microblaze_AVAILABLE=y
 CT_ARCH_mips_AVAILABLE=y
@@ -142,6 +138,7 @@ CT_ARCH_s390_AVAILABLE=y
 CT_ARCH_sh_AVAILABLE=y
 CT_ARCH_sparc_AVAILABLE=y
 CT_ARCH_x86_AVAILABLE=y
+CT_ARCH_xtensa_AVAILABLE=y
 CT_ARCH_SUFFIX=""
 
 #
@@ -154,6 +151,7 @@ CT_ARCH_ENDIAN="little"
 #
 # Target optimisations
 #
+CT_ARCH_EXCLUSIVE_WITH_CPU=y
 # CT_ARCH_FLOAT_AUTO is not set
 # CT_ARCH_FLOAT_SOFTFP is not set
 CT_ARCH_FLOAT="soft"
@@ -217,22 +215,14 @@ CT_TOOLCHAIN_ENABLE_NLS=y
 #
 CT_KERNEL_SUPPORTS_SHARED_LIBS=y
 CT_KERNEL="linux"
-CT_KERNEL_VERSION="custom"
+CT_KERNEL_VERSION="${LINUX_RELEASE}"
 # CT_KERNEL_bare_metal is not set
 CT_KERNEL_linux=y
 CT_KERNEL_bare_metal_AVAILABLE=y
 CT_KERNEL_linux_AVAILABLE=y
-# CT_KERNEL_V_4_0 is not set
-# CT_KERNEL_V_3_19 is not set
-# CT_KERNEL_V_3_18 is not set
-# CT_KERNEL_V_3_14 is not set
-# CT_KERNEL_V_3_12 is not set
-# CT_KERNEL_V_3_10 is not set
-# CT_KERNEL_V_3_4 is not set
-# CT_KERNEL_V_3_2 is not set
-# CT_KERNEL_V_2_6_32 is not set
 CT_KERNEL_LINUX_CUSTOM=y
 CT_KERNEL_LINUX_CUSTOM_LOCATION="${DOWNLOADDIR}/${LINUX_TARBALL}"
+CT_KERNEL_LINUX_CUSTOM_VERSION="${LINUX_RELEASE}"
 CT_KERNEL_windows_AVAILABLE=y
 
 #
@@ -259,30 +249,19 @@ CT_BINUTILS_binutils=y
 #
 # GNU binutils
 #
-# CT_CC_BINUTILS_SHOW_LINARO is not set
-CT_BINUTILS_V_2_25=y
-# CT_BINUTILS_V_2_24 is not set
-# CT_BINUTILS_V_2_23_2 is not set
-# CT_BINUTILS_V_2_23_1 is not set
-# CT_BINUTILS_V_2_22 is not set
-# CT_BINUTILS_V_2_21_53 is not set
-# CT_BINUTILS_V_2_21_1a is not set
-# CT_BINUTILS_V_2_20_1a is not set
-# CT_BINUTILS_V_2_19_1a is not set
-# CT_BINUTILS_V_2_18a is not set
 # CT_BINUTILS_CUSTOM is not set
-CT_BINUTILS_VERSION="2.25"
-CT_BINUTILS_2_25_or_later=y
+CT_BINUTILS_VERSION="2.24"
+# CT_CC_BINUTILS_SHOW_LINARO is not set
+# CT_BINUTILS_V_2_26 is not set
+# CT_BINUTILS_V_2_25_1 is not set
+CT_BINUTILS_V_2_24=y
+# CT_BINUTILS_V_2_23_2 is not set
 CT_BINUTILS_2_24_or_later=y
-CT_BINUTILS_2_23_or_later=y
-CT_BINUTILS_2_22_or_later=y
-CT_BINUTILS_2_21_or_later=y
-CT_BINUTILS_2_20_or_later=y
-CT_BINUTILS_2_19_or_later=y
-CT_BINUTILS_2_18_or_later=y
+CT_BINUTILS_2_23_2_or_later=y
 CT_BINUTILS_HAS_HASH_STYLE=y
 CT_BINUTILS_HAS_GOLD=y
 CT_BINUTILS_GOLD_SUPPORTS_ARCH=y
+CT_BINUTILS_GOLD_SUPPORT=y
 CT_BINUTILS_HAS_PLUGINS=y
 CT_BINUTILS_HAS_PKGVERSION_BUGURL=y
 CT_BINUTILS_LINKER_LD=y
@@ -309,6 +288,7 @@ CT_LIBC_VERSION="0.9.33.2"
 # CT_LIBC_glibc is not set
 # CT_LIBC_musl is not set
 CT_LIBC_uClibc=y
+CT_LIBC_avr_libc_AVAILABLE=y
 CT_LIBC_glibc_AVAILABLE=y
 CT_THREADS="nptl"
 CT_LIBC_mingw_AVAILABLE=y
@@ -316,19 +296,12 @@ CT_LIBC_musl_AVAILABLE=y
 CT_LIBC_newlib_AVAILABLE=y
 CT_LIBC_none_AVAILABLE=y
 CT_LIBC_uClibc_AVAILABLE=y
-CT_LIBC_UCLIBC_V_0_9_33_2=y
-# CT_LIBC_UCLIBC_V_0_9_33_1 is not set
-# CT_LIBC_UCLIBC_V_0_9_33 is not set
-# CT_LIBC_UCLIBC_V_0_9_32_1 is not set
-# CT_LIBC_UCLIBC_V_0_9_32 is not set
-# CT_LIBC_UCLIBC_V_0_9_31 is not set
-# CT_LIBC_UCLIBC_V_0_9_30_3 is not set
-# CT_LIBC_UCLIBC_V_0_9_30_2 is not set
-# CT_LIBC_UCLIBC_V_0_9_30_1 is not set
-# CT_LIBC_UCLIBC_V_0_9_30 is not set
 # CT_LIBC_UCLIBC_CUSTOM is not set
-CT_LIBC_UCLIBC_0_9_32_or_later=y
-CT_LIBC_UCLIBC_0_9_30_or_later=y
+# CT_LIBC_UCLIBC_CUSTOM_UCLIBC is not set
+# CT_LIBC_UCLIBC_CUSTOM_UCLIBC_NG is not set
+# CT_LIBC_UCLIBC_NG_V_1_0_12 is not set
+CT_LIBC_UCLIBC_V_0_9_33_2=y
+CT_LIBC_UCLIBC_0_9_33_2_or_later=y
 CT_LIBC_UCLIBC_PARALLEL=y
 CT_LIBC_UCLIBC_VERBOSITY_0=y
 # CT_LIBC_UCLIBC_VERBOSITY_1 is not set
@@ -358,6 +331,7 @@ CT_LIBC_XLDD=y
 #
 CT_LIBC_UCLIBC_LNXTHRD=""
 # CT_LIBC_UCLIBC_LOCALES is not set
+CT_LIBC_UCLIBC_IPV6=y
 CT_LIBC_UCLIBC_WCHAR=y
 CT_LIBC_UCLIBC_FENV=y
 
@@ -365,61 +339,18 @@ CT_LIBC_UCLIBC_FENV=y
 # C compiler
 #
 CT_CC="gcc"
-CT_CC_VERSION="4.9.2"
 CT_CC_CORE_PASSES_NEEDED=y
 CT_CC_CORE_PASS_1_NEEDED=y
 CT_CC_CORE_PASS_2_NEEDED=y
 CT_CC_gcc=y
-# CT_CC_GCC_SHOW_LINARO is not set
-# CT_CC_V_5_1 is not set
-CT_CC_V_4_9_2=y
-# CT_CC_V_4_9_1 is not set
-# CT_CC_V_4_9_0 is not set
-# CT_CC_V_4_8_4 is not set
-# CT_CC_V_4_8_3 is not set
-# CT_CC_V_4_8_2 is not set
-# CT_CC_V_4_8_1 is not set
-# CT_CC_V_4_8_0 is not set
-# CT_CC_V_4_7_4 is not set
-# CT_CC_V_4_7_3 is not set
-# CT_CC_V_4_7_2 is not set
-# CT_CC_V_4_7_1 is not set
-# CT_CC_V_4_7_0 is not set
-# CT_CC_V_4_6_4 is not set
-# CT_CC_V_4_6_3 is not set
-# CT_CC_V_4_6_2 is not set
-# CT_CC_V_4_6_1 is not set
-# CT_CC_V_4_6_0 is not set
-# CT_CC_V_4_5_3 is not set
-# CT_CC_V_4_5_2 is not set
-# CT_CC_V_4_5_1 is not set
-# CT_CC_V_4_5_0 is not set
-# CT_CC_V_4_4_7 is not set
-# CT_CC_V_4_4_6 is not set
-# CT_CC_V_4_4_5 is not set
-# CT_CC_V_4_4_4 is not set
-# CT_CC_V_4_4_3 is not set
-# CT_CC_V_4_4_2 is not set
-# CT_CC_V_4_4_1 is not set
-# CT_CC_V_4_4_0 is not set
-# CT_CC_V_4_3_6 is not set
-# CT_CC_V_4_3_5 is not set
-# CT_CC_V_4_3_4 is not set
-# CT_CC_V_4_3_3 is not set
-# CT_CC_V_4_3_2 is not set
-# CT_CC_V_4_3_1 is not set
-# CT_CC_V_4_2_4 is not set
-# CT_CC_V_4_2_2 is not set
-# CT_CC_CUSTOM is not set
-CT_CC_GCC_4_2_or_later=y
-CT_CC_GCC_4_3_or_later=y
-CT_CC_GCC_4_4_or_later=y
-CT_CC_GCC_4_5_or_later=y
-CT_CC_GCC_4_6_or_later=y
-CT_CC_GCC_4_7_or_later=y
+CT_CC_GCC_CUSTOM=y
+CT_CC_GCC_CUSTOM_LOCATION="${DOWNLOADDIR}/gcc-${GCC_VERSION}.tar.bz2"
+CT_CC_GCC_CUSTOM_VERSION="${GCC_VERSION}"
+CT_CC_GCC_VERSION="${GCC_VERSION}"
 CT_CC_GCC_4_8_or_later=y
-CT_CC_GCC_4_9=y
 CT_CC_GCC_4_9_or_later=y
+CT_CC_GCC_5_or_later=y
+CT_CC_GCC_latest=y
 CT_CC_GCC_HAS_GRAPHITE=y
 CT_CC_GCC_USE_GRAPHITE=y
 CT_CC_GCC_HAS_LTO=y
@@ -432,32 +363,10 @@ CT_CC_GCC_USE_MPC=y
 CT_CC_GCC_HAS_LIBQUADMATH=y
 CT_CC_GCC_HAS_LIBSANITIZER=y
 # CT_CC_LANG_FORTRAN is not set
-CT_CC_SUPPORT_CXX=y
-CT_CC_SUPPORT_FORTRAN=y
-CT_CC_SUPPORT_JAVA=y
-CT_CC_SUPPORT_ADA=y
-CT_CC_SUPPORT_OBJC=y
-CT_CC_SUPPORT_OBJCXX=y
-CT_CC_SUPPORT_GOLANG=y
-
-#
-# Additional supported languages:
-#
-CT_CC_LANG_CXX=y
-# CT_CC_LANG_JAVA is not set
-# CT_CC_LANG_ADA is not set
-# CT_CC_LANG_OBJC is not set
-# CT_CC_LANG_OBJCXX is not set
-# CT_CC_LANG_GOLANG is not set
-CT_CC_LANG_OTHERS=""
-
-#
-# gcc other options
-#
-CT_CC_ENABLE_CXX_FLAGS=""
-CT_CC_CORE_EXTRA_CONFIG_ARRAY=""
-CT_CC_EXTRA_CONFIG_ARRAY=""
-CT_CC_STATIC_LIBSTDCXX=y
+CT_CC_GCC_ENABLE_CXX_FLAGS=""
+CT_CC_GCC_CORE_EXTRA_CONFIG_ARRAY=""
+CT_CC_GCC_EXTRA_CONFIG_ARRAY=""
+CT_CC_GCC_STATIC_LIBSTDCXX=y
 # CT_CC_GCC_SYSTEM_ZLIB is not set
 
 #
@@ -490,6 +399,22 @@ CT_CC_GCC_DEC_FLOAT_AUTO=y
 # CT_CC_GCC_DEC_FLOAT_BID is not set
 # CT_CC_GCC_DEC_FLOAT_DPD is not set
 # CT_CC_GCC_DEC_FLOATS_NO is not set
+CT_CC_SUPPORT_CXX=y
+CT_CC_SUPPORT_FORTRAN=y
+CT_CC_SUPPORT_JAVA=y
+CT_CC_SUPPORT_ADA=y
+CT_CC_SUPPORT_OBJC=y
+CT_CC_SUPPORT_OBJCXX=y
+
+#
+# Additional supported languages:
+#
+CT_CC_LANG_CXX=y
+# CT_CC_LANG_JAVA is not set
+# CT_CC_LANG_ADA is not set
+# CT_CC_LANG_OBJC is not set
+# CT_CC_LANG_OBJCXX is not set
+CT_CC_LANG_OTHERS=""
 
 #
 # Debug facilities
@@ -514,13 +439,18 @@ CT_GDB_GDBSERVER_STATIC=y
 #
 # gdb version
 #
+# CT_GDB_CUSTOM is not set
+CT_GDB_VERSION="7.11"
 # CT_DEBUG_GDB_SHOW_LINARO is not set
+CT_GDB_V_7_11=y
+# CT_GDB_V_7_10_1 is not set
+# CT_GDB_V_7_10 is not set
 # CT_GDB_V_7_9_1 is not set
 # CT_GDB_V_7_9 is not set
 # CT_GDB_V_7_8_2 is not set
 # CT_GDB_V_7_8_1 is not set
 # CT_GDB_V_7_8 is not set
-CT_GDB_V_7_7_1=y
+# CT_GDB_V_7_7_1 is not set
 # CT_GDB_V_7_7 is not set
 # CT_GDB_V_7_6_1 is not set
 # CT_GDB_V_7_5_1 is not set
@@ -533,13 +463,11 @@ CT_GDB_V_7_7_1=y
 # CT_GDB_V_7_0_1a is not set
 # CT_GDB_V_7_0a is not set
 # CT_GDB_V_6_8a is not set
-# CT_GDB_CUSTOM is not set
 CT_GDB_7_2_or_later=y
 CT_GDB_7_0_or_later=y
 CT_GDB_HAS_PKGVERSION_BUGURL=y
 CT_GDB_HAS_PYTHON=y
 CT_GDB_INSTALL_GDBINIT=y
-CT_GDB_VERSION="7.7.1"
 CT_DEBUG_ltrace=y
 CT_LTRACE_V_0_7_3=y
 # CT_LTRACE_V_0_5_3 is not set
@@ -563,15 +491,18 @@ CT_COMPLIBS_NEEDED=y
 CT_GMP_NEEDED=y
 CT_MPFR_NEEDED=y
 CT_ISL_NEEDED=y
-CT_CLOOG_NEEDED=y
 CT_MPC_NEEDED=y
+CT_EXPAT_NEEDED=y
+CT_NCURSES_NEEDED=y
 CT_COMPLIBS=y
 CT_GMP=y
 CT_MPFR=y
 CT_ISL=y
-CT_CLOOG=y
 CT_MPC=y
 CT_LIBELF_TARGET=y
+CT_EXPAT=y
+CT_NCURSES=y
+# CT_GMP_V_6_1_0 is not set
 CT_GMP_V_6_0_0=y
 # CT_GMP_V_5_1_3 is not set
 # CT_GMP_V_5_1_1 is not set
@@ -582,6 +513,7 @@ CT_GMP_V_6_0_0=y
 # CT_GMP_V_4_3_0 is not set
 CT_GMP_5_0_2_or_later=y
 CT_GMP_VERSION="6.0.0a"
+# CT_MPFR_V_3_1_3 is not set
 CT_MPFR_V_3_1_2=y
 # CT_MPFR_V_3_1_0 is not set
 # CT_MPFR_V_3_0_1 is not set
@@ -592,12 +524,9 @@ CT_MPFR_V_3_1_2=y
 CT_MPFR_VERSION="3.1.2"
 # CT_ISL_V_0_14 is not set
 CT_ISL_V_0_12_2=y
-# CT_ISL_V_0_11_1 is not set
+CT_ISL_V_0_12_or_later=y
 CT_ISL_VERSION="0.12.2"
-CT_CLOOG_V_0_18_1=y
-# CT_CLOOG_V_0_18_0 is not set
-CT_CLOOG_VERSION="0.18.1"
-CT_CLOOG_0_18_or_later=y
+# CT_MPC_V_1_0_3 is not set
 CT_MPC_V_1_0_2=y
 # CT_MPC_V_1_0_1 is not set
 # CT_MPC_V_1_0 is not set
@@ -613,6 +542,11 @@ CT_MPC_VERSION="1.0.2"
 CT_LIBELF_V_0_8_13=y
 # CT_LIBELF_V_0_8_12 is not set
 CT_LIBELF_VERSION="0.8.13"
+CT_EXPAT_V_2_1_0=y
+CT_EXPAT_VERSION="2.1.0"
+CT_NCURSES_V_6_0=y
+CT_NCURSES_VERSION="6.0"
+# CT_NCURSES_NEW_ABI is not set
 
 #
 # Companion libraries common options

commit e93f7f158a975f431b329f32bd421d3985afc36e

images.make: include certain libraries only if necessary
Commit 44a1f46a758093338cc6450796bd1a72b27484b7 introduced a
regression where some libraries were unnecessarily included in the
final sysroot:

  commit 44a1f46a758093338cc6450796bd1a72b27484b7
  Author: Pankaj Bansal <pankaj.bansal@nxp.com>
  Date:   Sun Nov 6 20:40:04 2016 +0530
   
      Update Crosstool-NG from 1.21.0 to support Aarch64

This causes problems for some platforms that have very limited storage
space.

This patch reintroduces checks to see if the libraries are needed,
instead of just including them by default.

This is only done for the uClibc case, as it is assumed glibc users
are unconcerned about a few extra 100K bytes.

Signed-off-by: Curt Brune <curt@cumulusnetworks.com>

build-config/make/images.make
@@ -137,9 +137,18 @@ SYSROOT_LIBS	= ld$(CLIB64)-uClibc.so.0 ld$(CLIB64)-uClibc-$(XTOOLS_LIBC_VERSION)
 		  libc.so.0 libuClibc-$(XTOOLS_LIBC_VERSION).so \
 		  libcrypt.so.0 libcrypt-$(XTOOLS_LIBC_VERSION).so \
 		  libutil.so.0 libutil-$(XTOOLS_LIBC_VERSION).so \
-	     	  libdl.so.0 libdl-$(XTOOLS_LIBC_VERSION).so \
-		  libpthread.so.0 libpthread-$(XTOOLS_LIBC_VERSION).so \
+
+
+  ifeq ($(EXT3_4_ENABLE),yes)
+    SYSROOT_LIBS	+= \
+		  libdl.so.0 libdl-$(XTOOLS_LIBC_VERSION).so \
+		  libpthread.so.0 libpthread-$(XTOOLS_LIBC_VERSION).so
+  endif
+  # Add librt if ACPI or LVM2 is enabled
+  ifneq ($(filter yes, $(ACPI_ENABLE) $(LVM2_ENABLE)),)
+    SYSROOT_LIBS	+= \
 		  librt.so.0 librt-$(XTOOLS_LIBC_VERSION).so
+  endif
 else ifeq ($(XTOOLS_LIBC),glibc)
 SYSROOT_LIBS	= ld-$(XTOOLS_LIBC_VERSION).so \
 		  libm.so.6 libm-$(XTOOLS_LIBC_VERSION).so \