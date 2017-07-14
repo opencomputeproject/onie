50261b3898794970d6834491db6cb5e7e1e76147
Curt Brune <curt@cumulusnetworks.com>
2017-07-14 10:44:44 -0700

    docs: add favicon
    Signed-off-by: Curt Brune <curt@cumulusnetworks.com>

--- docs/conf.py
+++ docs/conf.py
@@ -138,7 +138,7 @@ html_logo = 'images/ONIE-logo-green-160x60.png'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = None
+html_favicon = 'images/onie-favicon-16x16.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
