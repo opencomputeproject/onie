53e97e15075f2a1399cb8109bdb49162c44e44ea
johnson.lu <johnson.lu@deltaww.com>
2017-09-15 11:35:16 +0800

    modify shell script machine.config backup error
    Signed-off-by: johnson.lu <johnson.lu@deltaww.com>

--- machine/delta/delta_ag9032v1/rootconf/sysroot-init/install-patch-set.sh
+++ machine/delta/delta_ag9032v1/rootconf/sysroot-init/install-patch-set.sh
@@ -3,7 +3,7 @@
 
 #Use machine-build.conf in place of machine.conf, because ONL install need machine.conf rather than machine-build.conf.
 if [ -r /etc/machine.conf ]; then
-    cp -a /etc/machine-build.conf /etc/machine-build.conf_backup
+    cp -a /etc/machine.conf /etc/machine.conf_backup
     cat /etc/machine-build.conf > /etc/machine.conf
     echo "onie_platform=$onie_platform" >> /etc/machine.conf
     echo "onie_machine=$onie_machine" >> /etc/machine.conf
