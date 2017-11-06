9354a9c4c5814fdf304a57c3ccf9a429d5f86463
Jonathan Tsai <jonathan.tsai@quantatw.com>
2017-11-06 13:49:44 +0800

    Update quanta_common_rangeley: gen-config-platform
    1. add gen-config-platform
    2. delete machine-detect.sh and S30machine-detect.sh
    
    Signed-off-by: Jonathan Tsai <jonathan.tsai@quantatw.com>
    Signed-off-by: Curt Brune <curt@cumulusnetworks.com>

--- machine/quanta/quanta_common_rangeley/rootconf/sysroot-init/machine-detect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-. /lib/onie/functions
-
-platform=$(/usr/bin/mb_detect -p)
-if [ -n "$(echo ${platform} | /bin/grep _rangeley)" ]; then
-	cp -f /etc/machine.conf /etc/machine_common_rangeley.conf
-	/bin/sed "s/common_rangeley/${platform}/g" /etc/machine_common_rangeley.conf > /etc/machine.conf
-	log_info_msg "Platform $(onie-sysinfo -m) detected ..."
-fi
