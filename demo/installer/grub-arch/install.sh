opencomputeproject/onie

commit a7b03d56b37afef1c734edcba996bc60973de09e

Add grub-2.02 with secure boot support
Move forward to GRUB version 2.02 plus several patches related to
secure boot and bug fixes.

The patches come from the Ubuntu grub2 source package:
http://archive.ubuntu.com/ubuntu/pool/main/g/grub2/grub2_2.02-2ubuntu2.debian.tar.xz

Most of the patches from Ubuntu are applied, except those related to
Ubuntu branding and unsupported CPU architectures.

Signed-off-by: Curt Brune <curt@cumulusnetworks.com>

build-config/scripts/mk-grub-efi-image
@@ -1,0 +1,96 @@
+#!/bin/sh
+
+#  Copyright (C) 2017 Curt Brune <curt@cumulusnetworks.com>
+#
+#  SPDX-License-Identifier:     GPL-2.0
+
+# Create monolithic GRUB EFI boot images for signing
+
+set -e
+
+ARCH="$1"
+GRUB_HOST_BIN_UEFI_DIR="$2"
+GRUB_TARGET_LIB_UEFI_DIR="$3"
+OUTPUT_IMAGE="$4"
+
+grub_mkimage="${GRUB_HOST_BIN_UEFI_DIR}/grub-mkimage"
+[ -x "$grub_mkimage" ] || {
+    echo "ERROR: Unable to find grub-mkimage: $grub_mkimage"
+    exit 1
+}
+
+[ -r "${GRUB_TARGET_LIB_UEFI_DIR}/efinet.mod" ] || {
+    echo "ERROR: Does not look like valid GRUB ${ARCH}-efi library directory: $GRUB_TARGET_LIB_UEFI_DIR"
+    exit 1
+}
+
+touch "$OUTPUT_IMAGE" || {
+    echo "ERROR: Unable to create output image: $OUTPUT_IMAGE"
+    exit 1
+}
+
+GRUB_MODULES="
+	all_video
+	boot
+	btrfs
+	cat
+	chain
+	configfile
+	echo
+	efifwsetup
+	efinet
+	ext2
+	fat
+	font
+	gettext
+	gfxmenu
+	gfxterm
+	gfxterm_background
+	gzio
+	halt
+	hfsplus
+	iso9660
+	jpeg
+	keystatus
+	loadenv
+	loopback
+	linux
+	linuxefi
+	lsefi
+	lsefimmap
+	lsefisystab
+	lssal
+	lvm
+	mdraid09
+	mdraid1x
+	memdisk
+	minicmd
+	normal
+	part_apple
+	part_msdos
+	part_gpt
+	password_pbkdf2
+	png
+	raid5rec
+	raid6rec
+	reboot
+	search
+	search_fs_uuid
+	search_fs_file
+	search_label
+	serial
+	sleep
+	squash4
+	terminal
+	terminfo
+	test
+	true
+	video
+	zfs
+	zfscrypt
+	zfsinfo
+"
+
+"$grub_mkimage" --format="${ARCH}-efi" --directory="$GRUB_TARGET_LIB_UEFI_DIR" \
+                --prefix="/EFI/onie" --output="$OUTPUT_IMAGE" \
+                $GRUB_MODULES

build-config/arch/armv8a.make
@@ -36,7 +36,6 @@ STRACE_ENABLE ?= yes
 
 # Disable Grub support by default. a machine make file can override this
 GRUB_ENABLE ?= no
-GRUB_IMAGE_NAME = grubaa64.efi
 
 # Disable UEFI support by default. a machine make file can override this
 UEFI_ENABLE ?= no

build-config/arch/x86_64.make
@@ -121,7 +121,6 @@ BTRFS_PROGS_ENABLE = yes
 
 # Include GRUB tools
 GRUB_ENABLE = yes
-GRUB_IMAGE_NAME = grubx64.efi
 
 # Default to include the i2ctools.  A particular machine.make can
 # override this.

build-config/make/grub.make
@@ -11,7 +11,7 @@
 # This is a makefile fragment that defines the build of grub
 #
 
-GRUB_VERSION		= 2.02~beta3
+GRUB_VERSION		= 2.02
 GRUB_TARBALL		= grub-$(GRUB_VERSION).tar.xz
 GRUB_TARBALL_URLS	+= $(ONIE_MIRROR) http://git.savannah.gnu.org/cgit/grub.git/snapshot/ ftp://alpha.gnu.org/gnu/grub/
 GRUB_BUILD_DIR		= $(USER_BUILDDIR)/grub
@@ -54,6 +54,8 @@ ifeq ($(FIRMWARE_TYPE),coreboot)
   GRUB_INSTALL_I386_COREBOOT_STAMP	= $(STAMPDIR)/grub-install-i386-coreboot
 endif
 
+GRUB_IMAGE_NAME		= grub$(EFI_ARCH).efi
+
 GRUB_STAMP		= $(GRUB_SOURCE_STAMP) \
 			  $(GRUB_PATCH_STAMP) \
 			  $(GRUB_CONFIGURE_I386_STAMP) \
@@ -69,6 +71,19 @@ GRUB_STAMP		= $(GRUB_SOURCE_STAMP) \
 			  $(GRUB_INSTALL_I386_COREBOOT_STAMP) \
 			  $(GRUB_INSTALL_STAMP)
 
+ifeq ($(SECURE_BOOT_ENABLE),yes)
+  GRUB_INSTALL_SB_STAMP	= $(STAMPDIR)/grub-install-sb
+  GRUB_MONOLITH_IMAGE	= $(MBUILDDIR)/grub$(EFI_ARCH).efi.unsigned
+  GRUB_SECURE_BOOT_IMAGE= $(MBUILDDIR)/grub$(EFI_ARCH).efi
+  GRUB_STAMP		+= $(GRUB_INSTALL_SB_STAMP)
+  UPDATER_IMAGE_PARTS   += $(GRUB_SECURE_BOOT_IMAGE)
+  UPDATER_IMAGE_PARTS_COMPLETE += $(GRUB_INSTALL_SB_STAMP)
+  PHONY			+= grub-install-sb
+  UEFI_BOOT_LOADER	= $(SHIM_IMAGE_NAME)
+else
+  UEFI_BOOT_LOADER	= $(GRUB_IMAGE_NAME)
+endif
+
 GRUB_TIMEOUT	?= 5
 
 # GRUB configuration options common to i386-pc and $(ARCH)-efi
@@ -81,8 +96,8 @@ GRUB_COMMON_CONFIG = 			\
 		--disable-grub-themes
 
 PHONY += grub grub-download grub-source grub-patch \
-	 grub-configure grub-build grub-install grub-clean \
-	 grub-download-clean
+	 grub-configure grub-build grub-install \
+	 grub-clean grub-download-clean
 
 GRUB_SBIN = grub-install grub-bios-setup grub-probe grub-reboot grub-set-default
 GRUB_BIN = grub-mkrelpath grub-mkimage grub-editenv
@@ -229,7 +244,7 @@ $(GRUB_INSTALL_STAMP): $(SYSROOT_INIT_STAMP) $(GRUB_INSTALL_I386_STAMP) \
 USER_CLEAN += grub-clean
 grub-clean:
 	$(Q) rm -rf $(GRUB_BUILD_DIR)
-	$(Q) rm -f $(GRUB_STAMP)
+	$(Q) rm -f $(GRUB_STAMP) $(GRUB_MONOLITH_IMAGE)
 	$(Q) echo "=== Finished making $@ for $(PLATFORM)"
 
 DOWNLOAD_CLEAN += grub-download-clean
@@ -345,6 +360,17 @@ grub-host-clean:
 	$(Q) rm -rf $(GRUB_HOST_BUILD_DIR)
 	$(Q) rm -f $(GRUB_HOST_STAMP)
 	$(Q) echo "=== Finished making $@ for $(PLATFORM)"
+
+grub-install-sb: $(GRUB_INSTALL_SB_STAMP)
+$(GRUB_INSTALL_SB_STAMP): $(SBSIGNTOOL_INSTALL_STAMP) $(GRUB_INSTALL_STAMP) $(GRUB_HOST_INSTALL_STAMP)
+	$(Q) echo "====  Building grub-$(ARCH)-efi-$(GRUB_VERSION) monolithic secure boot image ===="
+	$(Q) rm -rf $(SYSROOTDIR)/usr/lib/grub/$(ARCH)-efi
+	$(Q) $(SCRIPTDIR)/mk-grub-efi-image $(ARCH) $(GRUB_HOST_BIN_UEFI_DIR) \
+		$(GRUB_TARGET_LIB_UEFI_DIR) $(GRUB_MONOLITH_IMAGE)
+	$(Q) $(SBSIGN_EXE) --key $(ONIE_VENDOR_SECRET_KEY_PEM) \
+		--cert $(ONIE_VENDOR_CERT_PEM) \
+		--output $(GRUB_SECURE_BOOT_IMAGE) $(GRUB_MONOLITH_IMAGE)
+	$(Q) touch $@
 
 #-------------------------------------------------------------------------------
 #

patches/grub/2.02/bootp_process_dhcpack_http_boot.patch
@@ -1,0 +1,129 @@
+From a8af1751591a24f32ea0bed24da74b96f8d1ef9d Mon Sep 17 00:00:00 2001
+From: Michael Chang <mchang@suse.com>
+Date: Thu, 27 Oct 2016 17:42:19 -0400
+Subject: bootp: Add processing DHCPACK packet from HTTP Boot
+
+The vendor class identifier with the string "HTTPClient" is used to denote the
+packet as responding to HTTP boot request. In DHCP4 config, the filename for
+HTTP boot is the URL of the boot file while for PXE boot it is the path to the
+boot file. As a consequence, the next-server becomes obseleted because the HTTP
+URL already contains the server address for the boot file. For DHCP6 config,
+there's no difference definition in existing config as dhcp6.bootfile-url can
+be used to specify URL for both HTTP and PXE boot file.
+
+This patch adds processing for "HTTPClient" vendor class identifier in DHCPACK
+packet by treating it as HTTP format, not as the PXE format.
+
+Signed-off-by: Michael Chang <mchang@suse.com>
+Signed-off-by: Ken Lin <ken.lin@hpe.com>
+
+Patch-Name: bootp_process_dhcpack_http_boot.patch
+---
+ grub-core/net/bootp.c | 67 +++++++++++++++++++++++++++++++++++++++++++++++++--
+ include/grub/net.h    |  1 +
+ 2 files changed, 66 insertions(+), 2 deletions(-)
+
+diff --git a/grub-core/net/bootp.c b/grub-core/net/bootp.c
+index 172528ee8..de9239c16 100644
+--- a/grub-core/net/bootp.c
++++ b/grub-core/net/bootp.c
+@@ -207,6 +207,11 @@ parse_dhcp_vendor (const char *name, const void *vend, int limit, int *mask)
+                                      taglength);
+           break;
+ 
++        case GRUB_NET_BOOTP_VENDOR_CLASS_IDENTIFIER:
++          grub_env_set_net_property (name, "vendor_class_identifier", (const char *) ptr,
++                                     taglength);
++	  break;
++
+ 	case GRUB_NET_BOOTP_EXTENSIONS_PATH:
+           grub_env_set_net_property (name, "extensionspath", (const char *) ptr,
+                                      taglength);
+@@ -282,6 +287,66 @@ grub_net_configure_by_dhcp_ack (const char *name,
+     }
+ #endif
+ 
++  if (size > OFFSET_OF (vendor, bp))
++    {
++      char *cidvar;
++      const char *cid;
++
++      parse_dhcp_vendor (name, &bp->vendor, size - OFFSET_OF (vendor, bp), &mask);
++      cidvar = grub_xasprintf ("net_%s_%s", name, "vendor_class_identifier");
++      cid = grub_env_get (cidvar);
++      grub_free (cidvar);
++
++      if (cid && grub_strcmp (cid, "HTTPClient") == 0)
++	{
++	  char *proto, *ip, *pa;
++
++	  if (!dissect_url (bp->boot_file, &proto, &ip, &pa))
++	    return inter;
++
++	  grub_env_set_net_property (name, "boot_file", pa, grub_strlen (pa));
++	  if (is_def)
++	    {
++	      grub_net_default_server = grub_strdup (ip);
++	      grub_env_set ("net_default_interface", name);
++	      grub_env_export ("net_default_interface");
++	    }
++	  if (device && !*device)
++	    {
++	      *device = grub_xasprintf ("%s,%s", proto, ip);
++	      grub_print_error ();
++	    }
++	  if (path)
++	    {
++	      *path = grub_strdup (pa);
++	      grub_print_error ();
++	      if (*path)
++		{
++		  char *slash;
++		  slash = grub_strrchr (*path, '/');
++		  if (slash)
++		    *slash = 0;
++		  else
++		    **path = 0;
++		}
++	    }
++	  grub_net_add_ipv4_local (inter, mask);
++	  inter->dhcp_ack = grub_malloc (size);
++	  if (inter->dhcp_ack)
++	    {
++	      grub_memcpy (inter->dhcp_ack, bp, size);
++	      inter->dhcp_acklen = size;
++	    }
++	  else
++	    grub_errno = GRUB_ERR_NONE;
++
++	  grub_free (proto);
++	  grub_free (ip);
++	  grub_free (pa);
++	  return inter;
++	}
++    }
++
+   if (size > OFFSET_OF (boot_file, bp))
+     grub_env_set_net_property (name, "boot_file", bp->boot_file,
+                                sizeof (bp->boot_file));
+@@ -346,8 +411,6 @@ grub_net_configure_by_dhcp_ack (const char *name,
+ 	    **path = 0;
+ 	}
+     }
+-  if (size > OFFSET_OF (vendor, bp))
+-    parse_dhcp_vendor (name, &bp->vendor, size - OFFSET_OF (vendor, bp), &mask);
+   grub_net_add_ipv4_local (inter, mask);
+   
+   inter->dhcp_ack = grub_malloc (size);
+diff --git a/include/grub/net.h b/include/grub/net.h
+index 38a3973f3..e4bf678db 100644
+--- a/include/grub/net.h
++++ b/include/grub/net.h
+@@ -517,6 +517,7 @@ enum
+     GRUB_NET_BOOTP_DOMAIN = 0x0f,
+     GRUB_NET_BOOTP_ROOT_PATH = 0x11,
+     GRUB_NET_BOOTP_EXTENSIONS_PATH = 0x12,
++    GRUB_NET_BOOTP_VENDOR_CLASS_IDENTIFIER = 0x3C,
+     GRUB_NET_BOOTP_END = 0xff
+   };
+ 

patches/grub/2.02/core_in_fs.patch
@@ -1,0 +1,45 @@
+From a252840b98e8b67294738f197286d550d67b60f0 Mon Sep 17 00:00:00 2001
+From: Colin Watson <cjwatson@debian.org>
+Date: Mon, 13 Jan 2014 12:12:51 +0000
+Subject: Write marker if core.img was written to filesystem
+
+The Debian bug reporting script includes a warning in this case.
+
+Patch-Name: core_in_fs.patch
+---
+ util/setup.c | 8 ++++++++
+ 1 file changed, 8 insertions(+)
+
+diff --git a/util/setup.c b/util/setup.c
+index 8aa5a39a7..3b5c46fde 100644
+--- a/util/setup.c
++++ b/util/setup.c
+@@ -58,6 +58,8 @@
+ 
+ #include <errno.h>
+ 
++#define CORE_IMG_IN_FS		"setup_left_core_image_in_filesystem"
++
+ /* On SPARC this program fills in various fields inside of the 'boot' and 'core'
+  * image files.
+  *
+@@ -600,6 +602,8 @@ SETUP (const char *dir,
+ 
+     grub_free (sectors);
+ 
++    unlink (DEFAULT_DIRECTORY "/" CORE_IMG_IN_FS);
++
+     goto finish;
+   }
+ 
+@@ -642,6 +646,10 @@ unable_to_embed:
+   /* The core image must be put on a filesystem unfortunately.  */
+   grub_util_info ("will leave the core image on the filesystem");
+ 
++  fp = grub_util_fd_open (DEFAULT_DIRECTORY "/" CORE_IMG_IN_FS,
++			  GRUB_UTIL_FD_O_WRONLY);
++  grub_util_fd_close (fp);
++
+   grub_util_biosdisk_flush (root_dev->disk);
+ 
+   /* Clean out the blocklists.  */

patches/grub/2.02/disable-build-of-documentation.patch
@@ -1,0 +1,18 @@
+GRUB2: Disable build of documentation
+
+ONIE does not require the building of the documentation
+
+diff --git a/Makefile.am b/Makefile.am
+index 00a9663..93e1c0e 100644
+--- a/Makefile.am
++++ b/Makefile.am
+@@ -5,7 +5,7 @@ SUBDIRS = grub-core/gnulib .
+ if COND_real_platform
+ SUBDIRS += grub-core
+ endif
+-SUBDIRS += po docs util/bash-completion.d
++SUBDIRS += po util/bash-completion.d
+
+ include $(top_srcdir)/conf/Makefile.common
+ include $(top_srcdir)/conf/Makefile.extra-dist
+

commit e4120f26c833262aa972074ef426aeecb39c9389

demo: support UEFI secure boot installs
The recent work for UEFI secure boot broke how the DEMO OS installer
works.  The demo installer needed similar changes as the regular ONIE
installer to accommodate shimx64.efi and the monolithic grubx64.efi
binary.

In particular, the monolithic grubx64.efi binary with a hard coded
--prefix option was problematic to re-use in the DEMO OS context.

The fix here is to embed a grub.cfg into the monolithic image that
looks for another grub.cfg located in the same directory as the
grubx64.efi binary.

Fixes: a7b03d5, 0e56d23, b68ca41
Signed-off-by: Curt Brune <curt@cumulusnetworks.com>

build-config/scripts/mk-grub-efi-image
@@ -91,6 +91,15 @@ GRUB_MODULES="
 	zfsinfo
 "
 
+# Make an embedded config that reads a grub.cfg from the EFI directory
+# where the final grub binary is running.
+tmp_config=$(mktemp)
+cat <<EOF > $tmp_config
+configfile \$cmdpath/grub.cfg
+EOF
+
 "$grub_mkimage" --format="${ARCH}-efi" --directory="$GRUB_TARGET_LIB_UEFI_DIR" \
-                --prefix="/EFI/onie" --output="$OUTPUT_IMAGE" \
+                --prefix="/bogus" --config="$tmp_config" --output="$OUTPUT_IMAGE" \
                 $GRUB_MODULES
+
+rm -f $tmp_config

demo/installer/grub-arch/install.sh
@@ -253,26 +253,59 @@ demo_install_uefi_grub()
         exit 1
     }
 
-    grub_install_log=$(mktemp)
-    grub-install \
-        --no-nvram \
-        --bootloader-id="$demo_volume_label" \
-        --efi-directory="/boot/efi" \
-        --boot-directory="$demo_mnt" \
-        --recheck \
-        "$blk_dev" > /$grub_install_log 2>&1 || {
-        echo "ERROR: grub-install failed on: $blk_dev"
-        cat $grub_install_log && rm -f $grub_install_log
-        exit 1
-    }
-    rm -f $grub_install_log
+    if [ "$onie_secure_boot" = "yes" ] ; then
+        # ONIE is booting via shim, so the demo needs to also
+        local loader_dir="/boot/efi/EFI/$demo_volume_label"
+        mkdir -p "$loader_dir" || {
+            echo "ERROR: Unable to create directory: $loader_dir"
+            exit 1
+        }
+        # Use ONIE's .efi binaries
+        cp -a /boot/efi/EFI/onie/*${onie_uefi_arch}.efi "$loader_dir" || {
+            echo "ERROR: Unable to copy ONIE .efi binaries to: $loader_dir"
+            exit 1
+        }
+
+        local demo_boot_uuid=$(grub-probe --target=fs_uuid $demo_mnt) || {
+            echo "ERROR: Unable to determine UUID of GRUB boot directory: $demo_mnt"
+            return 1
+        }
+
+        # Generate tiny grub config for monolithic image
+        cat<< EOF > "${loader_dir}/grub.cfg"
+search.fs_uuid $demo_boot_uuid root
+echo "Search for uuid $demo_boot_uuid"
+ecoh "Found root: \$root"
+set prefix=(\$root)'/grub'
+configfile \$prefix/grub.cfg
+EOF
+
+        # Install primary grub config in $demo_mnt
+        grub_dir="${demo_mnt}/grub"
+        mkdir -p "${grub_dir}/fonts" "${grub_dir}/locale"
+    else
+        # Regular GRUB install
+        grub_install_log=$(mktemp)
+        grub-install \
+            --no-nvram \
+            --bootloader-id="$demo_volume_label" \
+            --efi-directory="/boot/efi" \
+            --boot-directory="$demo_mnt" \
+            --recheck \
+            "$blk_dev" > /$grub_install_log 2>&1 || {
+            echo "ERROR: grub-install failed on: $blk_dev"
+            cat $grub_install_log && rm -f $grub_install_log
+            exit 1
+        }
+        rm -f $grub_install_log
+    fi
 
     # Configure EFI NVRAM Boot variables.  --create also sets the
     # new boot number as active.
     efibootmgr --quiet --create \
         --label "$demo_volume_label" \
         --disk $blk_dev --part $uefi_part \
-        --loader "/EFI/$demo_volume_label/${onie_grub_image_name}" || {
+        --loader "/EFI/$demo_volume_label/$onie_uefi_boot_loader" || {
         echo "ERROR: efibootmgr failed to create new boot variable on: $blk_dev"
         exit 1
     }