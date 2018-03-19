opencomputeproject/onie

commit 073a103c0d09264870701bc769af7d5e8ce99370

add sbsigntool 0.6 for the host system
Build the sbsigntool executes for running on the build host.  These
are not cross-compiled, but are needed by the build system.

Signed-off-by: Curt Brune <curt@cumulusnetworks.com>

build-config/Makefile
@@ -335,6 +335,7 @@ ONIE_MIRROR	?= http://mirror.opencompute.org/onie
 # By default do not enable secure boot
 SECURE_BOOT_ENABLE ?= no
 ifeq ($(SECURE_BOOT_ENABLE),yes)
+  SBSIGNTOOL_ENABLE = yes
   PESIGN_ENABLE = yes
   GNU_EFI_ENABLE = yes
   OPENSSL_ENABLE = yes
@@ -351,6 +352,12 @@ ifeq ($(XTOOLS_ENABLE),yes)
   include make/xtools.make
 endif
 include make/sysroot.make
+ifeq ($(GNU_EFI_ENABLE),yes)
+  include make/gnu-efi.make
+endif
+ifeq ($(SBSIGNTOOL_ENABLE),yes)
+  include make/sbsigntool.make
+endif
 include make/kernel.make
 ifeq ($(UBOOT_ENABLE),yes)
   include make/u-boot.make
@@ -417,9 +424,6 @@ ifeq ($(IPMITOOL_ENABLE),yes)
 endif
 ifeq ($(PESIGN_ENABLE),yes)
   include make/pesign.make
-endif
-ifeq ($(GNU_EFI_ENABLE),yes)
-  include make/gnu-efi.make
 endif
 ifeq ($(OPENSSL_ENABLE),yes)
   include make/openssl.make

build-config/make/sbsigntool.make
@@ -1,0 +1,87 @@
+#-------------------------------------------------------------------------------
+#
+#  Copyright (C) 2018 Curt Brune <curt@cumulusnetworks.com>
+#
+#  SPDX-License-Identifier:     GPL-2.0
+#
+#-------------------------------------------------------------------------------
+#
+# This is a makefile fragment that defines the build of sbsigntool
+#
+
+SBSIGNTOOL_VERSION		= 0.6
+SBSIGNTOOL_TARBALL		= sbsigntool_$(SBSIGNTOOL_VERSION).orig.tar.gz
+SBSIGNTOOL_TARBALL_URLS		+= $(ONIE_MIRROR) http://http.debian.net/debian/pool/main/s/sbsigntool
+SBSIGNTOOL_BUILD_DIR		= $(USER_BUILDDIR)/sbsigntool
+SBSIGNTOOL_DIR			= $(SBSIGNTOOL_BUILD_DIR)/sbsigntool-$(SBSIGNTOOL_VERSION)
+
+SBSIGNTOOL_SRCPATCHDIR		= $(PATCHDIR)/sbsigntool
+SBSIGNTOOL_DOWNLOAD_STAMP	= $(DOWNLOADDIR)/sbsigntool-download
+SBSIGNTOOL_SOURCE_STAMP		= $(USER_STAMPDIR)/sbsigntool-source
+SBSIGNTOOL_CONFIGURE_STAMP	= $(USER_STAMPDIR)/sbsigntool-configure
+SBSIGNTOOL_BUILD_STAMP		= $(USER_STAMPDIR)/sbsigntool-build
+SBSIGNTOOL_INSTALL_STAMP	= $(STAMPDIR)/sbsigntool-install
+SBSIGNTOOL_STAMP		= $(SBSIGNTOOL_SOURCE_STAMP) \
+				  $(SBSIGNTOOL_CONFIGURE_STAMP) \
+				  $(SBSIGNTOOL_BUILD_STAMP) \
+				  $(SBSIGNTOOL_INSTALL_STAMP)
+
+SBSIGNTOOL_INSTALL_DIR		= $(SBSIGNTOOL_BUILD_DIR)/install
+SBSIGNTOOL_BIN_DIR		= $(SBSIGNTOOL_INSTALL_DIR)/bin
+SBSIGN_EXE			?= $(SBSIGNTOOL_BIN_DIR)/sbsign
+
+PHONY += sbsigntool sbsigntool-download sbsigntool-source sbsigntool-configure \
+	sbsigntool-build sbsigntool-install sbsigntool-clean sbsigntool-download-clean
+
+sbsigntool: $(SBSIGNTOOL_STAMP)
+
+DOWNLOAD += $(SBSIGNTOOL_DOWNLOAD_STAMP)
+sbsigntool-download: $(SBSIGNTOOL_DOWNLOAD_STAMP)
+$(SBSIGNTOOL_DOWNLOAD_STAMP): $(PROJECT_STAMP)
+	$(Q) rm -f $@ && eval $(PROFILE_STAMP)
+	$(Q) echo "==== Getting upstream sbsigntool ===="
+	$(Q) $(SCRIPTDIR)/fetch-package $(DOWNLOADDIR) $(UPSTREAMDIR) \
+		$(SBSIGNTOOL_TARBALL) $(SBSIGNTOOL_TARBALL_URLS)
+	$(Q) touch $@
+
+SOURCE += $(SBSIGNTOOL_SOURCE_STAMP)
+sbsigntool-source: $(SBSIGNTOOL_SOURCE_STAMP)
+$(SBSIGNTOOL_SOURCE_STAMP): $(USER_TREE_STAMP) $(SBSIGNTOOL_DOWNLOAD_STAMP)
+	$(Q) rm -f $@ && eval $(PROFILE_STAMP)
+	$(Q) echo "==== Extracting upstream sbsigntool ===="
+	$(Q) $(SCRIPTDIR)/extract-package $(SBSIGNTOOL_BUILD_DIR) $(DOWNLOADDIR)/$(SBSIGNTOOL_TARBALL)
+	$(Q) touch $@
+
+sbsigntool-configure: $(SBSIGNTOOL_CONFIGURE_STAMP)
+$(SBSIGNTOOL_CONFIGURE_STAMP): $(SBSIGNTOOL_SOURCE_STAMP) $(GNU_EFI_INSTALL_STAMP) | $(DEV_SYSROOT_INIT_STAMP)
+	$(Q) rm -f $@ && eval $(PROFILE_STAMP)
+	$(Q) echo "==== Configuring sbsigntool ===="
+	$(Q) cd $(SBSIGNTOOL_DIR) && $(SBSIGNTOOL_DIR)/configure \
+		--prefix=$(SBSIGNTOOL_INSTALL_DIR) \
+		CFLAGS="-I$(DEV_SYSROOT)/usr/include -I$(DEV_SYSROOT)/usr/include/efi -I$(DEV_SYSROOT)/usr/include/efi/x86_64"
+	$(Q) touch $@
+
+sbsigntool-build: $(SBSIGNTOOL_BUILD_STAMP)
+$(SBSIGNTOOL_BUILD_STAMP): $(SBSIGNTOOL_CONFIGURE_STAMP) | $(DEV_SYSROOT_INIT_STAMP)
+	$(Q) rm -f $@ && eval $(PROFILE_STAMP)
+	$(Q) echo "====  Building sbsigntool-$(SBSIGNTOOL_VERSION) ===="
+	$(Q) $(MAKE) -C $(SBSIGNTOOL_DIR) all
+	$(Q) touch $@
+
+sbsigntool-install: $(SBSIGNTOOL_INSTALL_STAMP)
+$(SBSIGNTOOL_INSTALL_STAMP): $(SYSROOT_INIT_STAMP) $(SBSIGNTOOL_BUILD_STAMP)
+	$(Q) rm -f $@ && eval $(PROFILE_STAMP)
+	$(Q) echo "==== Installing sbsigntool in $(DEV_SYSROOT) ===="
+	$(Q) rm -rf $(SBSIGNTOOL_INSTALL_DIR)
+	$(Q) $(MAKE) -C $(SBSIGNTOOL_DIR) install
+	$(Q) touch $@
+
+USERSPACE_CLEAN += sbsigntool-clean
+sbsigntool-clean:
+	$(Q) rm -rf $(SBSIGNTOOL_BUILD_DIR)
+	$(Q) rm -f $(SBSIGNTOOL_STAMP)
+	$(Q) echo "=== Finished making $@ for $(PLATFORM)"
+
+DOWNLOAD_CLEAN += sbsigntool-download-clean
+sbsigntool-download-clean:
+	$(Q) rm -f $(SBSIGNTOOL_DOWNLOAD_STAMP) $(DOWNLOADDIR)/$(SBSIGNTOOL_TARBALL)

upstream/sbsigntool_0.6.orig.tar.gz.sha1
@@ -1,0 +1,1 @@
+6e34f2e327e22ac7ca353f6643057e405c394c88  sbsigntool_0.6.orig.tar.gz

commit 9c44576b462cc879f6fa1a7370267ead8cc44ac0

Remove sbsigntool from host build
Instead of building sbsign for use on the build host, simply install
the sbsigntool package on the build host from Debian.

Add sbsigntool to $(DEBIAN_BUILD_HOST_PACKAGES), the list of packages
needed to setup a build host.

Fixes: 073a103c0d09 ("add sbsigntool 0.6 for the host system")
Signed-off-by: Curt Brune <curt@cumulusnetworks.com>

build-config/Makefile
@@ -335,7 +335,6 @@ ONIE_MIRROR	?= http://mirror.opencompute.org/onie
 # By default do not enable secure boot
 SECURE_BOOT_ENABLE ?= no
 ifeq ($(SECURE_BOOT_ENABLE),yes)
-  SBSIGNTOOL_ENABLE = yes
   PESIGN_ENABLE = yes
   GNU_EFI_ENABLE = yes
   OPENSSL_ENABLE = yes
@@ -355,9 +354,6 @@ endif
 include make/sysroot.make
 ifeq ($(GNU_EFI_ENABLE),yes)
   include make/gnu-efi.make
-endif
-ifeq ($(SBSIGNTOOL_ENABLE),yes)
-  include make/sbsigntool.make
 endif
 include make/kernel.make
 ifeq ($(UBOOT_ENABLE),yes)
@@ -518,7 +514,7 @@ DEBIAN_BUILD_HOST_PACKAGES	= build-essential stgit u-boot-tools util-linux \
 				  dosfstools mtools pkg-config git wget help2man libexpat1 \
 				  libexpat1-dev fakeroot python-sphinx rst2pdf \
 				  libefivar-dev libnss3-tools libnss3-dev libpopt-dev \
-				  libssl-dev efitools uuid-runtime
+				  libssl-dev sbsigntool uuid-runtime
 
 PHONY += debian-prepare-build-host
 debian-prepare-build-host:

build-config/make/grub.make
@@ -367,7 +367,7 @@ $(GRUB_INSTALL_SB_STAMP): $(SBSIGNTOOL_INSTALL_STAMP) $(GRUB_INSTALL_STAMP) $(GR
 	$(Q) rm -rf $(SYSROOTDIR)/usr/lib/grub/$(ARCH)-efi
 	$(Q) $(SCRIPTDIR)/mk-grub-efi-image $(ARCH) $(GRUB_HOST_BIN_UEFI_DIR) \
 		$(GRUB_TARGET_LIB_UEFI_DIR) $(GRUB_MONOLITH_IMAGE)
-	$(Q) $(SBSIGN_EXE) --key $(ONIE_VENDOR_SECRET_KEY_PEM) \
+	$(Q) sbsign --key $(ONIE_VENDOR_SECRET_KEY_PEM) \
 		--cert $(ONIE_VENDOR_CERT_PEM) \
 		--output $(GRUB_SECURE_BOOT_IMAGE) $(GRUB_MONOLITH_IMAGE)
 	$(Q) touch $@

build-config/make/images.make
@@ -438,7 +438,6 @@ $(RECOVERY_ISO_STAMP): $(GRUB_INSTALL_STAMP) $(GRUB_HOST_INSTALL_STAMP) $(RECOVE
 	     EXTRA_CMDLINE_LINUX="$(EXTRA_CMDLINE_LINUX)" \
 	     SERIAL_CONSOLE_ENABLE=$(SERIAL_CONSOLE_ENABLE) \
 	     SECURE_BOOT_ENABLE=$(SECURE_BOOT_ENABLE) \
-	     SBSIGN_EXE=$(SBSIGN_EXE) \
 	     SB_SHIM=$(SHIM_SECURE_BOOT_IMAGE) \
 	     ONIE_VENDOR_SECRET_KEY_PEM=$(ONIE_VENDOR_SECRET_KEY_PEM) \
 	     ONIE_VENDOR_CERT_PEM=$(ONIE_VENDOR_CERT_PEM) \

build-config/make/kernel.make
@@ -135,7 +135,7 @@ endif
 ifeq ($(SECURE_BOOT_ENABLE),yes)
 	$(Q) echo "====  Signing kernel secure boot image ===="
 	$(Q) cp -vf $(KERNEL_VMLINUZ) $(KERNEL_VMLINUZ).unsigned
-	$(Q) $(SBSIGN_EXE) --key $(ONIE_VENDOR_SECRET_KEY_PEM) \
+	$(Q) sbsign --key $(ONIE_VENDOR_SECRET_KEY_PEM) \
 		--cert $(ONIE_VENDOR_CERT_PEM) \
 		--output $(KERNEL_VMLINUZ) $(KERNEL_VMLINUZ).unsigned
 endif

build-config/make/sbsigntool.make
@@ -1,87 +1,0 @@
-#-------------------------------------------------------------------------------
-#
-#  Copyright (C) 2018 Curt Brune <curt@cumulusnetworks.com>
-#
-#  SPDX-License-Identifier:     GPL-2.0
-#
-#-------------------------------------------------------------------------------
-#
-# This is a makefile fragment that defines the build of sbsigntool
-#
-
-SBSIGNTOOL_VERSION		= 0.6
-SBSIGNTOOL_TARBALL		= sbsigntool_$(SBSIGNTOOL_VERSION).orig.tar.gz
-SBSIGNTOOL_TARBALL_URLS		+= $(ONIE_MIRROR) http://http.debian.net/debian/pool/main/s/sbsigntool
-SBSIGNTOOL_BUILD_DIR		= $(USER_BUILDDIR)/sbsigntool
-SBSIGNTOOL_DIR			= $(SBSIGNTOOL_BUILD_DIR)/sbsigntool-$(SBSIGNTOOL_VERSION)
-
-SBSIGNTOOL_SRCPATCHDIR		= $(PATCHDIR)/sbsigntool
-SBSIGNTOOL_DOWNLOAD_STAMP	= $(DOWNLOADDIR)/sbsigntool-download
-SBSIGNTOOL_SOURCE_STAMP		= $(USER_STAMPDIR)/sbsigntool-source
-SBSIGNTOOL_CONFIGURE_STAMP	= $(USER_STAMPDIR)/sbsigntool-configure
-SBSIGNTOOL_BUILD_STAMP		= $(USER_STAMPDIR)/sbsigntool-build
-SBSIGNTOOL_INSTALL_STAMP	= $(STAMPDIR)/sbsigntool-install
-SBSIGNTOOL_STAMP		= $(SBSIGNTOOL_SOURCE_STAMP) \
-				  $(SBSIGNTOOL_CONFIGURE_STAMP) \
-				  $(SBSIGNTOOL_BUILD_STAMP) \
-				  $(SBSIGNTOOL_INSTALL_STAMP)
-
-SBSIGNTOOL_INSTALL_DIR		= $(SBSIGNTOOL_BUILD_DIR)/install
-SBSIGNTOOL_BIN_DIR		= $(SBSIGNTOOL_INSTALL_DIR)/bin
-SBSIGN_EXE			?= $(SBSIGNTOOL_BIN_DIR)/sbsign
-
-PHONY += sbsigntool sbsigntool-download sbsigntool-source sbsigntool-configure \
-	sbsigntool-build sbsigntool-install sbsigntool-clean sbsigntool-download-clean
-
-sbsigntool: $(SBSIGNTOOL_STAMP)
-
-DOWNLOAD += $(SBSIGNTOOL_DOWNLOAD_STAMP)
-sbsigntool-download: $(SBSIGNTOOL_DOWNLOAD_STAMP)
-$(SBSIGNTOOL_DOWNLOAD_STAMP): $(PROJECT_STAMP)
-	$(Q) rm -f $@ && eval $(PROFILE_STAMP)
-	$(Q) echo "==== Getting upstream sbsigntool ===="
-	$(Q) $(SCRIPTDIR)/fetch-package $(DOWNLOADDIR) $(UPSTREAMDIR) \
-		$(SBSIGNTOOL_TARBALL) $(SBSIGNTOOL_TARBALL_URLS)
-	$(Q) touch $@
-
-SOURCE += $(SBSIGNTOOL_SOURCE_STAMP)
-sbsigntool-source: $(SBSIGNTOOL_SOURCE_STAMP)
-$(SBSIGNTOOL_SOURCE_STAMP): $(USER_TREE_STAMP) $(SBSIGNTOOL_DOWNLOAD_STAMP)
-	$(Q) rm -f $@ && eval $(PROFILE_STAMP)
-	$(Q) echo "==== Extracting upstream sbsigntool ===="
-	$(Q) $(SCRIPTDIR)/extract-package $(SBSIGNTOOL_BUILD_DIR) $(DOWNLOADDIR)/$(SBSIGNTOOL_TARBALL)
-	$(Q) touch $@
-
-sbsigntool-configure: $(SBSIGNTOOL_CONFIGURE_STAMP)
-$(SBSIGNTOOL_CONFIGURE_STAMP): $(SBSIGNTOOL_SOURCE_STAMP) $(GNU_EFI_INSTALL_STAMP) | $(DEV_SYSROOT_INIT_STAMP)
-	$(Q) rm -f $@ && eval $(PROFILE_STAMP)
-	$(Q) echo "==== Configuring sbsigntool ===="
-	$(Q) cd $(SBSIGNTOOL_DIR) && $(SBSIGNTOOL_DIR)/configure \
-		--prefix=$(SBSIGNTOOL_INSTALL_DIR) \
-		CFLAGS="-I$(DEV_SYSROOT)/usr/include -I$(DEV_SYSROOT)/usr/include/efi -I$(DEV_SYSROOT)/usr/include/efi/x86_64"
-	$(Q) touch $@
-
-sbsigntool-build: $(SBSIGNTOOL_BUILD_STAMP)
-$(SBSIGNTOOL_BUILD_STAMP): $(SBSIGNTOOL_CONFIGURE_STAMP) | $(DEV_SYSROOT_INIT_STAMP)
-	$(Q) rm -f $@ && eval $(PROFILE_STAMP)
-	$(Q) echo "====  Building sbsigntool-$(SBSIGNTOOL_VERSION) ===="
-	$(Q) $(MAKE) -C $(SBSIGNTOOL_DIR) all
-	$(Q) touch $@
-
-sbsigntool-install: $(SBSIGNTOOL_INSTALL_STAMP)
-$(SBSIGNTOOL_INSTALL_STAMP): $(SYSROOT_INIT_STAMP) $(SBSIGNTOOL_BUILD_STAMP)
-	$(Q) rm -f $@ && eval $(PROFILE_STAMP)
-	$(Q) echo "==== Installing sbsigntool in $(DEV_SYSROOT) ===="
-	$(Q) rm -rf $(SBSIGNTOOL_INSTALL_DIR)
-	$(Q) $(MAKE) -C $(SBSIGNTOOL_DIR) install
-	$(Q) touch $@
-
-USERSPACE_CLEAN += sbsigntool-clean
-sbsigntool-clean:
-	$(Q) rm -rf $(SBSIGNTOOL_BUILD_DIR)
-	$(Q) rm -f $(SBSIGNTOOL_STAMP)
-	$(Q) echo "=== Finished making $@ for $(PLATFORM)"
-
-DOWNLOAD_CLEAN += sbsigntool-download-clean
-sbsigntool-download-clean:
-	$(Q) rm -f $(SBSIGNTOOL_DOWNLOAD_STAMP) $(DOWNLOADDIR)/$(SBSIGNTOOL_TARBALL)

build-config/scripts/onie-mk-iso.sh
@@ -93,22 +93,23 @@ fi
 }
 
 if [ "$SECURE_BOOT_ENABLE" = "yes" ] ; then
-   [ -x "$SBSIGN_EXE" ]  || {
-       echo "ERROR: Unable to find sbsign utility: $SBSIGN_EXE"
-       exit 1
-   }
-   [ -r "$SB_SHIM" ]  || {
-       echo "ERROR: Unable to read secure boot signed shim image: $SB_SHIM"
-       exit 1
-   }
-   [ -r "$ONIE_VENDOR_SECRET_KEY_PEM" ]  || {
-       echo "ERROR: Unable to read ONIE_VENDOR_SECRET_KEY_PEM file: $ONIE_VENDOR_SECRET_KEY_PEM"
-       exit 1
-   }
-   [ -r "$ONIE_VENDOR_CERT_PEM" ]  || {
-       echo "ERROR: Unable to read ONIE_VENDOR_CERT_PEM file: $ONIE_VENDOR_CERT_PEM"
-       exit 1
-   }
+    which sbsign > /dev/null 2>&1 || {
+        echo "ERROR: Unable to find sbsign utility: sbsign"
+        echo "ERROR: Check that package sbsigntool is installed on your system"
+        exit 1
+    }
+    [ -r "$SB_SHIM" ]  || {
+        echo "ERROR: Unable to read secure boot signed shim image: $SB_SHIM"
+        exit 1
+    }
+    [ -r "$ONIE_VENDOR_SECRET_KEY_PEM" ]  || {
+        echo "ERROR: Unable to read ONIE_VENDOR_SECRET_KEY_PEM file: $ONIE_VENDOR_SECRET_KEY_PEM"
+        exit 1
+    }
+    [ -r "$ONIE_VENDOR_CERT_PEM" ]  || {
+        echo "ERROR: Unable to read ONIE_VENDOR_CERT_PEM file: $ONIE_VENDOR_CERT_PEM"
+        exit 1
+    }
 fi
 
 # Make sure a few tools are available
@@ -256,7 +257,7 @@ if [ "$UEFI_ENABLE" = "yes" ] ; then
     # UEFI loader image(s) into it.
     if [ "$SECURE_BOOT_ENABLE" = "yes" ] ; then
         # sign the grub image with the onie vendor key
-        "$SBSIGN_EXE" --key $ONIE_VENDOR_SECRET_KEY_PEM --cert $ONIE_VENDOR_CERT_PEM \
+        sbsign --key $ONIE_VENDOR_SECRET_KEY_PEM --cert $ONIE_VENDOR_CERT_PEM \
                --output $RECOVERY_EFI_GRUBX86_IMG $RECOVERY_GRUBX86_IMG
         # copy shim into place as the loader
         cp $SB_SHIM $RECOVERY_EFI_BOOTX86_IMG

upstream/sbsigntool_0.6.orig.tar.gz.sha1
@@ -1,1 +1,0 @@
-6e34f2e327e22ac7ca353f6643057e405c394c88  sbsigntool_0.6.orig.tar.gz