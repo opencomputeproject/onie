opencomputeproject/onie

commit 0e56d239cc51f5674068a99f7452ca5a4178e05c

installer: updates for secure boot
Signed-off-by: Curt Brune <curt@cumulusnetworks.com>

build-config/make/images.make
@@ -290,9 +290,13 @@ $(SYSROOT_COMPLETE_STAMP): $(SYSROOT_CHECK_STAMP)
 	$(Q) echo "onie_firmware=$(FIRMWARE_TYPE)" >> $(MACHINE_CONF)
 	$(Q) echo "onie_switch_asic=$(SWITCH_ASIC_VENDOR)" >> $(MACHINE_CONF)
 	$(Q) echo "onie_skip_ethmgmt_macs=$(SKIP_ETHMGMT_MACS)" >> $(MACHINE_CONF)
-       ifeq ($(GRUB_ENABLE),yes)
-	  $(Q) echo "onie_grub_image_name=$(GRUB_IMAGE_NAME)" >> $(MACHINE_CONF)
-       endif
+ifeq ($(UEFI_ENABLE),yes)
+	$(Q) echo "onie_uefi_boot_loader=$(UEFI_BOOT_LOADER)" >> $(MACHINE_CONF)
+	$(Q) echo "onie_uefi_arch=$(EFI_ARCH)" >> $(MACHINE_CONF)
+endif
+ifeq ($(SECURE_BOOT_ENABLE),yes)
+	$(Q) echo "onie_secure_boot=$(SECURE_BOOT_ENABLE)" >> $(MACHINE_CONF)
+endif
 	$(Q) cp $(LSB_RELEASE_FILE) $(SYSROOTDIR)/etc/lsb-release
 	$(Q) cp $(OS_RELEASE_FILE) $(SYSROOTDIR)/etc/os-release
 	$(Q) cp $(MACHINE_CONF) $(SYSROOTDIR)/etc/machine-build.conf
@@ -364,7 +368,7 @@ $(IMAGE_UPDATER_STAMP): $(UPDATER_IMAGE_PARTS_COMPLETE) $(UPDATER_IMAGE_PARTS_PL
 	     UPDATER_UBOOT_NAME=$(UPDATER_UBOOT_NAME) \
 	     EXTRA_CMDLINE_LINUX="$(EXTRA_CMDLINE_LINUX)" \
 	     SERIAL_CONSOLE_ENABLE=$(SERIAL_CONSOLE_ENABLE) \
-	     UEFI_BOOT_LOADER=$(GRUB_IMAGE_NAME) \
+	     UEFI_BOOT_LOADER=$(UEFI_BOOT_LOADER) \
 	     $(SCRIPTDIR)/onie-mk-installer.sh onie $(ROOTFS_ARCH) $(MACHINEDIR) \
 		$(MACHINE_CONF) $(INSTALLER_DIR) \
 		$(UPDATER_IMAGE) $(UPDATER_IMAGE_PARTS) $(UPDATER_IMAGE_PARTS_PLATFORM)

installer/grub-arch/install-arch
@@ -616,8 +616,9 @@ install_grub()
 
 }
 
-# UEFI BIOS GRUB install function.
-# Installs GRUB in the specified location.
+
+# UEFI traditional GRUB install function.  Installs GRUB in the
+# specified location.
 #
 # arg $1 -- EFI root directory
 # arg $2 -- install device
@@ -645,6 +646,80 @@ install_uefi_grub()
         exit 1
     }
     rm -f $grub_install_log
+}
+
+# UEFI shim and GRUB install function.  Installs GRUB and the shim
+# binaries in the specified location.
+#
+# arg $1 -- EFI root directory
+# arg $2 -- install device
+# arg $3 -- GRUB boot directory
+
+install_uefi_shim_grub()
+{
+    local uefi_dir="$1"
+    local boot_dev="$2"
+    local boot_dir="$3"
+
+    [ "$verbose" = "yes" ] && echo "Installing UEFI SHIM, GRUB in device: $uefi_dir, boot-directory: $boot_dir"
+
+    local loader_dir="${uefi_dir}/EFI/onie"
+
+    mkdir -p "$loader_dir" || {
+        echo "ERROR: Unable to create directory: $loader_dir"
+        return 1
+    }
+
+    # copy shim and grub binaries into loader directory
+    for f in shim mm grub ; do
+        local loader="${f}${image_uefi_arch}.efi"
+        cp "$loader" $loader_dir || {
+            echo "ERROR: Unable to copy loader file: $loader to loader_dir: $loader_dir"
+            return 1
+        }
+    done
+
+    local onie_boot_uuid=$(grub-probe --target=fs_uuid $boot_dir) || {
+        echo "ERROR: Unable to determine UUID of GRUB boot directory: $boot_dir"
+        return 1
+    }
+
+    # Generate tiny grub config for monolithic image
+    cat<< EOF > "${loader_dir}/grub.cfg"
+search.fs_uuid $onie_boot_uuid root hd0,gpt$onie_boot_part
+set prefix=(\$root)'/grub'
+configfile \$prefix/grub.cfg
+EOF
+
+    # Install primary grub config in $onie_boot_part
+    grub_dir="${boot_dir}/grub"
+    mkdir -p "${grub_dir}/fonts" "${grub_dir}/locale"
+
+}
+
+
+# UEFI loader install function.
+#
+# arg $1 -- EFI root directory
+# arg $2 -- install device
+# arg $3 -- GRUB boot directory
+install_uefi_loader()
+{
+    local uefi_dir="$1"
+    local boot_dev="$2"
+    local boot_dir="$3"
+
+    if [ "$image_secure_boot" = "yes" ] ; then
+        install_uefi_shim_grub "$uefi_dir" "$boot_dev" "$boot_dir" || {
+            echo "ERROR: problems installing shim and grub on: $boot_dev"
+            exit 1
+        }
+    else
+        install_uefi_grub "$uefi_dir" "$boot_dev" "$boot_dir" || {
+            echo "ERROR: problems installing grub on: $boot_dev"
+            exit 1
+        }
+    fi
 
     if [ "$onie_boot_reason" = "embed" ] ; then
         # Configure EFI NVRAM Boot variables.  --create also sets the
@@ -652,7 +727,7 @@ install_uefi_grub()
         efibootmgr --quiet --create \
             --label "ONIE: Open Network Install Environment" \
             --disk $boot_dev --part $uefi_esp_part \
-            --loader /EFI/onie/${onie_grub_image_name} || {
+            --loader /EFI/onie/${image_uefi_boot_loader} || {
             echo "ERROR: efibootmgr failed to create new boot variable on: $boot_dev"
             exit 1
         }
@@ -753,8 +828,8 @@ install_image()
     }
 
     if [ "$install_firmware" = "uefi" ] ; then
-        install_uefi_grub $uefi_esp_mnt $onie_dev $onie_boot_mnt || {
-            echo "Error: Problems installing UEFI GRUB on device: $uefi_esp_mnt, $onie_dev, $onie_boot_mnt"
+        install_uefi_loader $uefi_esp_mnt $onie_dev $onie_boot_mnt || {
+            echo "Error: Problems installing UEFI loader on device: $uefi_esp_mnt, $onie_dev, $onie_boot_mnt"
             exit 1
         }
     else

commit f390c32202dd5e5f385d7e1ca907fd6f2b32bf6b

machine.conf: add onie_grub_image_name for old NOS installers
In ONIE v2018.05, the variable is renamed to `onie_uefi_boot_loader`.

The previous version of demo OS installers or some old NOS installers
cannot register correct EFI boot entry without `onie_grub_image_name`.
With the incorrect boot entry, it will fall back to ONIE environment
directly.

The patch addes `onie_grub_image_name` for legacy NOS/demo OS installers
to create correct boot entry.

The patch has been tested on Accton AS5712_54X and AS5912_54X.

Fixes: 0e56d23

build-config/make/images.make
@@ -292,6 +292,7 @@ $(SYSROOT_COMPLETE_STAMP): $(SYSROOT_CHECK_STAMP)
 	$(Q) echo "onie_switch_asic=$(SWITCH_ASIC_VENDOR)" >> $(MACHINE_CONF)
 	$(Q) echo "onie_skip_ethmgmt_macs=$(SKIP_ETHMGMT_MACS)" >> $(MACHINE_CONF)
 ifeq ($(UEFI_ENABLE),yes)
+	$(Q) echo "onie_grub_image_name=$(UEFI_BOOT_LOADER)" >> $(MACHINE_CONF)
 	$(Q) echo "onie_uefi_boot_loader=$(UEFI_BOOT_LOADER)" >> $(MACHINE_CONF)
 	$(Q) echo "onie_uefi_arch=$(EFI_ARCH)" >> $(MACHINE_CONF)
 endif