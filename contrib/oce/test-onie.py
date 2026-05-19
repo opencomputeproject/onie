4f60bd1657a4507d9c13a7db2dcdfd16cf7219d6
Sai Nallapaneni <supra.nlpn@gmail.com>
2026-05-19 13:38:09 -0400

    build: migrate environment and scripts to python 3
    Debian 12 and other modern host operating systems have removed Python 2. This commit updates the host preparation dependencies to use modern python3 equivalents and updates legacy python shebangs across the build and contrib scripts to ensure compatibility.
    
    Fixes build-host preparation failures related to python-all-dev and python-sphinx.
    
    Tested by: Successfully running 'make debian-prepare-build-host' and verifying the toolchain extraction phase completes on WSL Ubuntu 22.04.
    
    Signed-off-by: Sai Nallapaneni <supra.nlpn@gmail.com>

--- contrib/oce/test-onie.py
+++ contrib/oce/test-onie.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # -----------------------------------------------------------------------------
 # Copyright (C) 2014-2016 Carlos Cardenas <carlos@cumulusnetworks.com>
